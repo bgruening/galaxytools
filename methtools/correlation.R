ca8e9ea1e4a43b3632baa1a05a6a0ed2640d37e7
Björn Grüning <bjoern.gruening@gmail.com>
2013-07-13 13:02:40 +0200

    add some more tweaks and a svg output

--- methtools/correlation.R
+++ methtools/correlation.R
@@ -7,6 +7,7 @@ print(args)
 
 kdepairs <- function(x, n=25, density=TRUE, contour=TRUE, ...) {
     require(MASS)
+    library(gplots)
     y <- data.frame(x)
     fun.lower <- function(x1, x2, ...) {
         if (is.factor(x1)) {
@@ -22,7 +23,10 @@ kdepairs <- function(x, n=25, density=TRUE, contour=TRUE, ...) {
             if (density || contour)
                 d <- kde2d(x1, x2, n=n)
             if (density)
-                image(d, col=rainbow(1000, s = 1, v = 1, start = 0, end = max(1, n - 1)/n, alpha = 1), add=TRUE)
+                palette( rev(rich.colors(512)) )
+                #image(d, col=rainbow(1000, s = 1, v = 1, start = 0, end = max(1, n - 1)/n, alpha = 1), add=TRUE)
+                image(d, col=palette(), add=TRUE)
+                #image(d, col=1 + 31*d$z, add=TRUE)
             if (contour)
                 # parameter hier: http://www.inside-r.org/r-doc/graphics/contour
                 graphics:::contour(d,add=TRUE, nlevels = 15, labcex = 0.6, drawlabels=FALSE, lwd=0)
@@ -50,19 +54,155 @@ kdepairs <- function(x, n=25, density=TRUE, contour=TRUE, ...) {
         rect(breaks[-nB], 0, breaks[-1], y, col="gold", ...)
         box()
     }
-    pairs.default(y, lower.panel=fun.lower, upper.panel=fun.upper, diag.panel=panel.hist)
+    pairs2(y, lower.panel=fun.lower, upper.panel=fun.upper, diag.panel=panel.hist)
     invisible(NULL)
 }
+
+#http://stackoverflow.com/questions/9680783/how-can-i-change-the-axis-position-for-pairs
+pairs2 <- 
+  function (x, labels, panel = points, ..., lower.panel = panel, 
+            upper.panel = panel, diag.panel = NULL, text.panel = textPanel, 
+            label.pos = 0.5 + has.diag/3, cex.labels = NULL, font.labels = 1, 
+            row1attop = TRUE, gap = 1) 
+  {
+    textPanel <- function(x = 0.5, y = 0.5, txt, cex, font) text(x, 
+                                                                 y, txt, cex = cex, font = font)
+    localAxis <- function(side, x, y, xpd, bg, col = NULL, main, 
+                          oma, ...) {
+      if (side%%2 == 1) 
+        Axis(x, side = side, xpd = NA, ...)
+      else Axis(y, side = side, xpd = NA, ...)
+    }
+    localPlot <- function(..., main, oma, font.main, cex.main) plot(...)
+    localLowerPanel <- function(..., main, oma, font.main, cex.main) lower.panel(...)
+    localUpperPanel <- function(..., main, oma, font.main, cex.main) upper.panel(...)
+    localDiagPanel <- function(..., main, oma, font.main, cex.main) diag.panel(...)
+    dots <- list(...)
+    nmdots <- names(dots)
+    if (!is.matrix(x)) {
+      x <- as.data.frame(x)
+      for (i in seq_along(names(x))) {
+        if (is.factor(x[[i]]) || is.logical(x[[i]])) 
+          x[[i]] <- as.numeric(x[[i]])
+        if (!is.numeric(unclass(x[[i]]))) 
+          stop("non-numeric argument to 'pairs'")
+      }
+    }
+    else if (!is.numeric(x)) 
+      stop("non-numeric argument to 'pairs'")
+    panel <- match.fun(panel)
+    if ((has.lower <- !is.null(lower.panel)) && !missing(lower.panel)) 
+      lower.panel <- match.fun(lower.panel)
+    if ((has.upper <- !is.null(upper.panel)) && !missing(upper.panel)) 
+      upper.panel <- match.fun(upper.panel)
+    if ((has.diag <- !is.null(diag.panel)) && !missing(diag.panel)) 
+      diag.panel <- match.fun(diag.panel)
+    if (row1attop) {
+      tmp <- lower.panel
+      lower.panel <- upper.panel
+      upper.panel <- tmp
+      tmp <- has.lower
+      has.lower <- has.upper
+      has.upper <- tmp
+    }
+    nc <- ncol(x)
+    if (nc < 2) 
+      stop("only one column in the argument to 'pairs'")
+    has.labs <- TRUE
+    if (missing(labels)) {
+      labels <- colnames(x)
+      if (is.null(labels)) 
+        labels <- paste("var", 1L:nc)
+    }
+    else if (is.null(labels)) 
+      has.labs <- FALSE
+    oma <- if ("oma" %in% nmdots) 
+      dots$oma
+    else NULL
+    main <- if ("main" %in% nmdots) 
+      dots$main
+    else NULL
+    if (is.null(oma)) {
+      oma <- c(4, 4, 4, 4)
+      if (!is.null(main)) 
+        oma[3L] <- 6
+    }
+    opar <- par(mfrow = c(nc, nc), mar = rep.int(gap/2, 4), oma = oma)
+    on.exit(par(opar))
+    dev.hold()
+    on.exit(dev.flush(), add = TRUE)
+    for (i in if (row1attop) 
+      1L:nc
+         else nc:1L) for (j in 1L:nc) {
+           localPlot(x[, j], x[, i], xlab = "", ylab = "", axes = FALSE, 
+                     type = "n", ...)
+           if (i == j || (i < j && has.lower) || (i > j && has.upper)) {
+             box()
+             # edited here...
+             #           if (i == 1 && (!(j%%2) || !has.upper || !has.lower)) 
+             #           localAxis(1 + 2 * row1attop, x[, j], x[, i], 
+             #                       ...)
+             # draw x-axis
+             if (i == nc & j != nc) 
+               localAxis(1, x[, j], x[, i], 
+                         ...)
+             # draw y-axis
+             if (j == 1 & i != 1) 
+               localAxis(2, x[, j], x[, i], ...)
+             #           if (j == nc && (i%%2 || !has.upper || !has.lower)) 
+             #             localAxis(4, x[, j], x[, i], ...)
+             mfg <- par("mfg")
+             if (i == j) {
+               if (has.diag) 
+                 localDiagPanel(as.vector(x[, i]), ...)
+               if (has.labs) {
+                 par(usr = c(0, 1, 0, 1))
+                 if (is.null(cex.labels)) {
+                   l.wid <- strwidth(labels, "user")
+                   cex.labels <- max(0.8, min(2, 0.9/max(l.wid)))
+                 }
+                 text.panel(0.5, label.pos, labels[i], cex = cex.labels, 
+                            font = font.labels)
+               }
+             }
+             else if (i < j) 
+               localLowerPanel(as.vector(x[, j]), as.vector(x[, 
+                                                              i]), ...)
+             else localUpperPanel(as.vector(x[, j]), as.vector(x[, 
+                                                                 i]), ...)
+             if (any(par("mfg") != mfg)) 
+               stop("the 'panel' function made a new plot")
+           }
+           else par(new = FALSE)
+         }
+    if (!is.null(main)) {
+      font.main <- if ("font.main" %in% nmdots) 
+        dots$font.main
+      else par("font.main")
+      cex.main <- if ("cex.main" %in% nmdots) 
+        dots$cex.main
+      else par("cex.main")
+      mtext(main, 3, 3, TRUE, 0.5, cex = cex.main, font = font.main)
+    }
+    invisible(NULL)
+  }
 
 
 infile <- args[1]
 outfile <- args[2]
 is_header <- as.logical(args[3])
+is_svg <- as.logical(args[4])
+
 
 #mydat<-read.table("/home/bag/Downloads/correltion_test/combined.out")
 mydat<-read.table(infile, header = is_header, sep="\t")
-
-png(outfile, pointsize = 22, width=1000, height=1000)
+if (is_svg) {
+    #library(RSvgDevice)  
+    #devSVG(file=outfile,  width = 7, height = 7, onefile=TRUE)
+    svg(outfile, width = 7, height = 7, pointsize = 11)
+}else {
+    png(outfile, pointsize = 4, width=2000, height=2000, res=600)
+}
 kdepairs(mydat, n=100, density=TRUE, contour=TRUE)
 dev.off()
 
