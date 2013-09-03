26c474a706b053f640f37223f04ec1c60bafc050
Pavankumar Videm <videmp@bipc37.informatik.uni-freiburg.de>
2013-09-03 17:34:00 +0200

    fixed the intermediate file saving hack

--- deseq2/deseq2.R
+++ deseq2/deseq2.R
@@ -28,96 +28,99 @@ opt$samples<-trim(opt$samples)
 
 htseqCountTable = read.table(opt$input, sep="\t", comment="", as.is=T)
 
-l<-unique(c(htseqCountTable[1,-1]))
-#print(l)
-
-colnames(htseqCountTable) <- htseqCountTable[1,]
+colnames(htseqCountTable) <- htseqCountTable[2,]
 names(htseqCountTable)<-colnames(htseqCountTable)
+conditions <- htseqCountTable[1,]
+conditions <- unlist(conditions[,-1])
+attr(htseqCountTable,"conditions") <- conditions
 tagnames <- htseqCountTable[-(1:2), 1]
 htseqCountTable <- htseqCountTable[-(1:2),-1]
 htseqCountTable[,1] <- gsub(",", ".", htseqCountTable[,1])
 
+l <- unique(c(conditions))
 
 library( "DESeq2" )
 
-opt$samples
-
-print('----------------')
-
 pdf(opt$plots)
-
 if (opt$samples=="all_vs_all"){
   # all versus all
   for (i in seq(1, length(l), by=1)) {
     k=i+1
     if(k<=length(l)){
       for (j in seq(k, length(l), by=1)) {
-        
-        colData<-names(htseqCountTable[, which(gsub("[.][0-9]+","",names(htseqCountTable)) %in% c(l[[i]], l[[j]]))])
-        currentPairTable<-htseqCountTable[ , which(gsub("[.][0-9]+","",names(htseqCountTable)) %in% c(l[[i]], l[[j]]))]
-        #      rownames(currentPairTable)<-tagnames
-        
-        write.table(currentPairTable, file=paste(l[[i]],"_", l[[j]],".txt"))
-        currentPairTable<-read.table(paste(l[[i]],"_", l[[j]],".txt"),row.names=1,header=T)
-        
-        currentPairTable <- as.data.frame(currentPairTable,stringsAsFactors=F)
-        #print(currentPairTable)
-        pdata = data.frame(condition=factor(c(rep(l[[i]], 2), rep(l[[j]], 2))),row.names=colnames(currentPairTable))
+	print(paste(l[i]," ",l[j]))
+	
+	currentColmuns <- which(conditions %in% c(l[[i]], l[[j]]))
+        sampleNames <- names(htseqCountTable[currentColmuns])
+
+	currentPairTable <- htseqCountTable[currentColmuns]
+	ncondition1cols <- length(which(conditions %in% c(l[[i]])))        
+	ncondition2cols <- length(which(conditions %in% c(l[[i]])))
+
+	ntabcols <- length(currentPairTable)
+        currentPairTable <- as.integer(unlist(currentPairTable))
+	currentPairTable <- matrix(unlist(currentPairTable), ncol = ntabcols, byrow = FALSE)
+        colnames(currentPairTable) <- sampleNames
+
+	pdata = data.frame(condition=factor(conditions[currentColmuns]),row.names=colnames(currentPairTable))
+	print(pdata)
         dds = DESeqDataSetFromMatrix(countData = currentPairTable,  colData = pdata, design = ~ condition)
-        
-        #      colData(dds)$condition
+
         dds <- DESeq(dds)
         sizeFactors(dds)
         plotDispEsts(dds)
         plotMA(dds)
         
         res <- results(dds)
-        rownames(res)<-tagnames
-        resSorted <- res[order(res$padj),];
-        #column.names=FALSE
-        write.csv( as.data.frame(resSorted), file = opt$outputfile, sep="\t")
+	resCols <- colnames(res)
+
+	cond <- c(rep(paste(unique(conditions[currentColmuns]),collapse="_"), nrow(res)))
+	res[, "condition"] <- cond
+	res[, "geneIds"] <- tagnames
+	
+        resSorted <- res[order(res$padj),]
+        write.table(as.data.frame(resSorted[,c("condition", "geneIds", resCols)]), file = opt$outputfile, sep="\t", quote = FALSE, append=TRUE, row.names = FALSE, col.names = FALSE)
       }
     }
   }
 }else{
 
   sampleSets<-unlist(strsplit(opt$samples, " "))
-  sampleSets
   samplesControl <- {}
   samplesExperiment <- {}
-  sampleNames <- {}
-
   samplesControl<-unlist(strsplit(sampleSets[1], ","))
   samplesExperiment<-unlist(strsplit(sampleSets[2], ","))
 
-  print(samplesControl)
-  print(samplesExperiment)
-
   # the minus one is needed because we get column indizes including the first column
-  samplecolumns <- c()
-  for (i in samplesControl) samplecolumns[[length(samplecolumns) + 1]] <- as.integer(i) - 1
-  for (i in samplesExperiment) samplecolumns[[length(samplecolumns) + 1]] <- as.integer(i) - 1
+  sampleColumns <- c()
+  for (i in samplesControl) sampleColumns[[length(sampleColumns) + 1]] <- as.integer(i) - 1
+  for (i in samplesExperiment) sampleColumns[[length(sampleColumns) + 1]] <- as.integer(i) - 1
   
-print(samplecolumns)
-  
-  htseqSubCountTable <- htseqCountTable[,samplecolumns]
+  htseqSubCountTable <- htseqCountTable[,sampleColumns]
 
-  #TODO: pavan hack :)
-  #write.table(htseqSubCountTable, file=paste(gsub(" ","_",opt$samples),".txt"))
-  #htseqSubCountTable<-read.table(paste(gsub(" ","_",opt$samples),".txt"),row.names=1,header=T)
+  ntabcols <- length(htseqSubCountTable)
+  htseqSubCountTable <- as.integer(unlist(htseqSubCountTable))
+  htseqSubCountTable <- matrix(unlist(htseqSubCountTable), ncol = ntabcols, byrow = FALSE)
+  colnames(htseqSubCountTable) <- names(htseqCountTable)[sampleColumns]
 
-  pdata = data.frame(condition=factor(c(names( htseqSubCountTable )[ samplecolumns ])),row.names=colnames(htseqSubCountTable))
-  dds = DESeqDataSetFromMatrix(countData = bla,  colData = pdata, design = ~ condition)
-  
+  pdata = data.frame(condition=factor(conditions[sampleColumns]),row.names=colnames(htseqSubCountTable))
+  print(pdata)
+  dds = DESeqDataSetFromMatrix(countData = htseqSubCountTable,  colData = pdata, design = ~ condition)
+
   dds <- DESeq(dds)
   sizeFactors(dds)
   plotDispEsts(dds)
   plotMA(dds)
   
   res <- results(dds)
-  rownames(res)<-tagnames
-  resSorted <- res[order(res$padj),];
-  #column.names=FALSE
-  write.csv( as.data.frame(resSorted), file = opt$outputfile, sep="\t")
+  resCols <- colnames(res)
+
+  cond <- c(rep(paste(unique(conditions[sampleColumns]),collapse="_"), nrow(res)))
+  res[, "condition"] <- cond
+  res[, "geneIds"] <- tagnames
+
+  resSorted <- res[order(res$padj),]
+  write.table(as.data.frame(resSorted[,c("condition", "geneIds", resCols)]), file = opt$outputfile, sep="\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
 dev.off()
+
