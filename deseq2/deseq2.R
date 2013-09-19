613cd21570be185c3861cbbc894c10682647b368
Pavankumar Videm <videmp@bipc37.informatik.uni-freiburg.de>
2013-09-19 10:15:20 +0200

    Plot names added

--- deseq2/deseq2.R
+++ deseq2/deseq2.R
@@ -170,8 +170,8 @@ if (opt$samples=="all_vs_all"){
     write.table(as.data.frame(resSorted[,c("condition", "geneIds", resCols)]), file = opt$outputfile, sep="\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
     if ( !is.null(opt$plots) ) {
-        plotDispEsts(dds)
-        plotMA(dds)
+            plotDispEsts(dds, main="Dispersion estimate plot")
+            plotMA(dds, main="MA-plot")
 
 		library("RColorBrewer")
 		library("gplots")
