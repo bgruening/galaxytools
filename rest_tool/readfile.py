53113e65e217598a5627729f8338d0cc45a4e968
Bernhard Lutz <Bernhard-Lutz@online.de>
2014-05-03 00:01:34 +0200

    add post-data

--- rest_tool/readfile.py
+++ rest_tool/readfile.py
@@ -17,7 +17,16 @@ def getresult(url):
     else:
         return connection.read().rstrip()
         
-def store_result(url, outfile):
+def store_result_get(url, outfile):
     data=getresult(url)
     outfile.write(data)
+    outfile.close()
+
+def store_result_post(url, post, outfile):
+    data = urllib.urlencode(post)
+    headers={"Content-Type" : "application/x-www-form-urlencoded"}
+    req = urllib2.Request(url, data, headers)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    outfile.write(the_page)
     outfile.close()
--- rest_tool/rest_tool_advanced.py
+++ rest_tool/rest_tool_advanced.py
@@ -7,10 +7,12 @@ import readfile
 
 #dicitionary for the output format
 
-dict_output={"cids" :"txt", "aids" : "txt", "sids" : "txt", "description": "xml", "summary" : "xml", "record" : "csv", "classification": "xml", "targets" : "txt", "xrefs" : "txt", "synonyms" : "txt", "property": "csv" }
+dict_output={"cids" :"txt", "aids" : "txt", "sids" : "txt", "description": "xml", "summary" : "xml", "record" : "csv", "classification": "xml", "targets" : "txt", "xrefs" : "txt", "synonyms" : "txt", "property": "csv", "doseresponse" : "csv" }
 
 #alles andere ist xml
 check_for_id_type=["cids", "aids", "sids"]
+
+post_id_types=["inchi", "sdf", "smiles"]
 
 id_dict={"compound": "cid", "assay": "aid", "substance" : "sid" }
 
@@ -19,31 +21,43 @@ def getListString(args):
         #build comma list
         list_string=",".join(getListFromFile(open(args.id_value,"r")))
     else:
-        list_string=args.id_value
+        print (args.id_value)
+        list_string=args.id_value.strip().replace("__cr____cn__", ",")
     return list_string
 
-
+    
 def main(args):
-
     url="http://pubchem.ncbi.nlm.nih.gov/rest/pug/"+args.type+"/"
     url+=args.id_type+"/"
-    if args.id_type ==id_dict[args.type]:
-        url+=getListString(args)+"/"
-    else:
-        url+=args.id_value+"/"
+    # check if we are post then skip this part otherwise put the ids in the url
+    if not args.id_type in post_id_types:
+        if args.id_type ==id_dict[args.type]:
+            url+=getListString(args)+"/"
+        else:
+            url+=args.id_value+"/"
+
     url+=args.operation+"/"
     if args.operation == "target" or args.operation == "property" or args.operation == "xrefs":
         url+=args.operation_value+"/"
     
-    url+=dict_output[args.operation]
-    if args.operation in check_for_id_type:
-            url+="?"+args.operation+"_type="+args.ids_operation_type
+    if args.operation == "xrefs":
+        if "," in args.operation_value:
+            url+="xml"
+        else:
+            url+="txt"
+    else:
+        url+=dict_output[args.operation]
+    if args.operation in check_for_id_type and args.id_type not in post_id_types:
+            url+="?%s_type=%s" % (args.operation, args.ids_operation_type)
     print(url)
-    readfile.store_result(url, args.outfile)
-    
-    
-    
-    
+    if args.id_type in post_id_types:
+        postfile=open(args.id_value,"r")
+        post_value=postfile.read()
+        post_dict={args.id_type : post_value}
+        print(post_dict)
+        readfile.store_result_post(url, post_dict, args.outfile)
+    else:
+        readfile.store_result_get(url, args.outfile)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', type=str, required=True,
