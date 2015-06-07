9e5550fe3eb5bf3d1ef06c20ed135ea1495718ea
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-06-07 23:26:47 +0200

    Update script to Eric's comments.

--- tools/protein_properties/protein_properties.py
+++ tools/protein_properties/protein_properties.py
@@ -4,21 +4,20 @@ import sys
 from Bio import SeqIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
-with open(sys.argv[2], 'w+') as result:
+sys.stdout.write("ID\tMW\tIP\tgravy\tlength\tinstability\tmonoisotpoic\tSequence\n")
 
-    result.write("ID\tSequence\tMW\tIP\tgravy\tlength\tinstability\tmonoisotpoic\n")
-    for record in SeqIO.parse(open(sys.argv[1]), "fasta"):
-        a = ProteinAnalysis(str(record.seq))
+for record in SeqIO.parse(sys.stdin, "fasta"):
+    a = ProteinAnalysis(str(record.seq))
 
-        properties = list()
-        properties.append(record.id)
-        properties.append(a.sequence)
-        properties.append(a.molecular_weight())
-        properties.append(a.isoelectric_point())
-        properties.append(a.gravy())
-        properties.append(a.length)
-        properties.append(a.instability_index())
-        properties.append(a.aromaticity())
-
-        result.write( '\t'.join(map(str, properties))+"\n" )
+    properties = list()
+    properties.append(record.id)
+    properties.append(a.molecular_weight())
+    properties.append(a.isoelectric_point())
+    properties.append(a.gravy())
+    properties.append(a.length)
+    properties.append(a.instability_index())
+    properties.append(a.aromaticity())
+    # always last column to make the output more readable
+    properties.append(a.sequence)
+    sys.stdout.write( '\t'.join(map(str, properties))+"\n" )
 
