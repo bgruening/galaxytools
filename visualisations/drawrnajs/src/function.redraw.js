1e9595ae2bcec76739db012acdf7f10b39fde1f4
y.hoogstrate@erasmusmc.nl <y.hoogstrate@erasmusmc.nl>
2015-06-11 17:09:46 +0200

    Textual changes and a fix that now allows dot-bracket files to show the visulization icon

--- visualisations/drawrnajs/src/function.redraw.js
+++ visualisations/drawrnajs/src/function.redraw.js
@@ -1,43 +1,43 @@
 
 function redraw(sequence,structure)
 {
-	var rna = require("drawrnajs");
-	var app = rna.vis;
-	
-	while(structure.length < sequence.length)
-	{
-		structure += '.';
-	}
-	
-	document.getElementById('SEQ_BOX').value = sequence;
-	document.getElementById('DOTBR_BOX').value = structure;
-	
-	//init colors
-	$("#acolor").spectrum({ color: "#64F73F" });
-	$("#ccolor").spectrum({ color: "#FFB340" });
-	$("#gcolor").spectrum({ color: "#EB413C" });
-	$("#ucolor").spectrum({ color: "#3C88EE" });
-	$("#selcolor").spectrum({color: "#F6F6F6"});
+    var rna = require("drawrnajs");
+    var app = rna.vis;
+    
+    while(structure.length < sequence.length)
+    {
+        structure += '.';
+    }
+    
+    document.getElementById('SEQ_BOX').value = sequence;
+    document.getElementById('DOTBR_BOX').value = structure;
+    
+    //init colors
+    $("#acolor").spectrum({ color: "#64F73F" });
+    $("#ccolor").spectrum({ color: "#FFB340" });
+    $("#gcolor").spectrum({ color: "#EB413C" });
+    $("#ucolor").spectrum({ color: "#3C88EE" });
+    $("#selcolor").spectrum({color: "#F6F6F6"});
 
-	//init alert box
-	document.getElementById('ALERT').value = "";
+    //init alert box
+    document.getElementById('ALERT').value = "";
 
-	var input = rna.io.getInputSequences();
-	var struct = rna.t.transformDotBracket(input[0], input[1]);
+    var input = rna.io.getInputSequences();
+    var struct = rna.t.transformDotBracket(input[0], input[1]);
 
-	var cy = document.getElementById('cy');
-	cy.style.width = "60%";
+    var cy = document.getElementById('cy');
+    cy.style.width = "60%";
 
-	app({graph: struct, el: cy, doc: document, win: window});
+    app({graph: struct, el: cy, doc: document, win: window});
 
-	var runButton = document.getElementById('PERFORM_VIS');
-	runButton.readOnly = true;
-	runButton.addEventListener('click', function(){ 
-		document.getElementById('ALERT').value = "";
-		var input = rna.io.getInputSequences();
-		if(rna.io.checkConditions(input)){
-			struct = rna.t.transformDotBracket(input[0], input[1]);
-			app({graph: struct, el: cy, doc: document, win: window});
-		}
-	}, false);
+    var runButton = document.getElementById('PERFORM_VIS');
+    runButton.readOnly = true;
+    runButton.addEventListener('click', function(){ 
+        document.getElementById('ALERT').value = "";
+        var input = rna.io.getInputSequences();
+        if(rna.io.checkConditions(input)){
+            struct = rna.t.transformDotBracket(input[0], input[1]);
+            app({graph: struct, el: cy, doc: document, win: window});
+        }
+    }, false);
 }
