bc9099f9f96c1c34040921016023c33cc88aa0f1
Greg Von Kuster <ghv2@psu.edu>
2015-06-18 14:49:25 -0400

    Add the imagej2_bunwarpj_elastic_transform tool, which results in slight refactoring for the imagej2_bunwarpj_align tool due to the introduction of the new jython_utils.py file.  This file will soon have additional functions.  This commit also includes a fix for the imagej2_bunwarpj_align tool config - max_subsamp_fact had a value and min of 1 when it should have been 0.  A fix for the associated .shed.yml file is also included (improper indenting of remote_repository_url).

--- tools/image_processing/imagej2/imagej2_bunwarpj_align/jython_script.py
+++ tools/image_processing/imagej2/imagej2_bunwarpj_align/jython_script.py
@@ -1,5 +1,5 @@
 import sys
-import imagej2_base_utils
+import jython_utils
 from ij import IJ
 
 # Fiji Jython interpreter implements Python 2.5 which does not
@@ -23,30 +23,15 @@ else:
     target_datatype = sys.argv[ -3 ]
     target_path = sys.argv[ -2 ]
 
-def convert_before_saving_as_tiff( registered_image ):
-    # The bUnwarpJ macro produces tiff image stacks consisting of 3
-    # slices which can be viewed in ImageJ.  The 3 slices are::
-    # 1) the registered image, 2) the target image and 3) the black/white
-    # warp image.  Galaxy supports only single-layered images, so we have
-    # to convert the image to something other than tiff so that slices are
-    # eliminated and we can then convert back to tiff for saving.  This is
-    # kind of a hack - there is probably a way to do this without converting
-    # twice.  I messed around with the the ImageJ getSlice() method, but
-    # couldn't get it to produce what is needed here.
-    tmp_dir = imagej2_base_utils.get_temp_dir()
-    tmp_out_png_path = imagej2_base_utils.get_temporary_image_path( tmp_dir, 'png' )
-    IJ.saveAs( registered_image, 'png', tmp_out_png_path )
-    return IJ.openImage( tmp_out_png_path )
-
 # Save the Registered Source Image.
 registered_source_image = IJ.openImage( source_tiff_path )
 if source_datatype == 'tiff':
-    registered_source_image = convert_before_saving_as_tiff( registered_source_image )
+    registered_source_image = jython_utils.convert_before_saving_as_tiff( registered_source_image )
 IJ.saveAs( registered_source_image, source_datatype, source_path )
 
 if not mono:
     # Save the Registered Target Image.
     registered_target_image = IJ.openImage( target_tiff_path )
     if target_datatype == 'tiff':
-        registered_target_image = convert_before_saving_as_tiff( registered_target_image )
+        registered_target_image = jython_utils.convert_before_saving_as_tiff( registered_target_image )
     IJ.saveAs( registered_target_image, target_datatype, target_path )
