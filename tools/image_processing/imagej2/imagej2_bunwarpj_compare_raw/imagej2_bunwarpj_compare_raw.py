2612adbda59bcad5078a54e965fadfc80781ca1e
Greg Von Kuster <ghv2@psu.edu>
2015-06-22 10:53:03 -0400

    Add the imagej2_bunwarpj_compare_raw tool.

--- tools/image_processing/imagej2/imagej2_bunwarpj_compare_raw/imagej2_bunwarpj_compare_raw.py
+++ tools/image_processing/imagej2/imagej2_bunwarpj_compare_raw/imagej2_bunwarpj_compare_raw.py
@@ -12,8 +12,8 @@ parser.add_argument( '--source_image', dest='source_image', help='Source image' 
 parser.add_argument( '--source_image_format', dest='source_image_format', help='Source image format' )
 parser.add_argument( '--target_image', dest='target_image', help='Target image' )
 parser.add_argument( '--target_image_format', dest='target_image_format', help='Target image format' )
-parser.add_argument( '--source_transformation', dest='source_transformation', default=None, help='Direct source transformation matrix' )
-parser.add_argument( '--target_transformation', dest='target_transformation', default=None, help='Inverse target transformation matrix' )
+parser.add_argument( '--raw_transformation1', dest='raw_transformation1', default=None, help='First raw transformation matrix' )
+parser.add_argument( '--raw_transformation2', dest='raw_transformation2', default=None, help='Second raw transformation matrix' )
 parser.add_argument( '--max_heap_size_type', dest='max_heap_size_type', help='Type (default or megabytes) of max_heap_size value' )
 parser.add_argument( '--max_heap_size', dest='max_heap_size', help='Maximum size of the memory allocation pool used by the JVM.' )
 parser.add_argument( '--output', dest='output', help='Warping index' )
@@ -23,8 +23,8 @@ args = parser.parse_args()
 tmp_dir = imagej2_base_utils.get_temp_dir()
 source_image_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.source_image, args.source_image_format )
 target_image_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.target_image, args.target_image_format )
-source_transformation_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.source_transformation, 'txt' )
-target_transformation_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.target_transformation, 'txt' )
+raw_transformation1_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.raw_transformation1, 'txt' )
+raw_transformation2_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.raw_transformation2, 'txt' )
 # bUnwarpJ produces several lines of output that we need to discard, so
 # we'll use a temporary output file from which we'll read only the last line.
 tmp_output_path = imagej2_base_utils.get_input_image_path( tmp_dir, args.output, 'txt' )
@@ -42,15 +42,14 @@ tmp_stderr = open( tmp_err, 'wb' )
 cmd = imagej2_base_utils.get_base_cmd_bunwarpj( memory_size )
 if cmd is None:
     imagej2_base_utils.stop_err( "bUnwarpJ not found!" )
-cmd += ' -compare_elastic'
-# Target is sent before source.
+cmd += ' -compare_raw'
 cmd += ' %s' % target_image_path
 cmd += ' %s' % source_image_path
-cmd += ' %s' % target_transformation_path
-cmd += ' %s' % source_transformation_path
+cmd += ' %s' % raw_transformation1_path
+cmd += ' %s' % raw_transformation2_path
 cmd += ' > %s' % tmp_output_path
 
-# Calculate the warping index of two elastic transformations using bUnwarpJ.
+# Calculate the warping index of two raw transformations using bUnwarpJ.
 proc = subprocess.Popen( args=cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True )
 rc = proc.wait()
 if rc != 0:
@@ -58,11 +57,11 @@ if rc != 0:
     imagej2_base_utils.stop_err( error_message )
 
 # Example contents of tmp_output_path:
-# ['Target image : ~/tmpKAYF1P.jpg\n',
-#  'Source image : ~/tmpgQX0dy.gif\n',
-#  'Target Transformation file : ~/tmpZJC_4B.txt\n',
-#  'Source Transformation file : ~/tmphsSojl.txt\n',
-#  ' Warping index = 14.87777347388348\n']
+# ['Target image : ~/tmp5WmDku.jpg\n',
+#  'Source image : ~/tmps74U40.gif\n',
+#  'Target Transformation file : ~/tmpXofC1x.txt\n',
+#  'Source Transformation file : ~/tmpFqNYe4.txt\n',
+#  ' Warping index = 24.111209027033937\n']
 results = open( tmp_output_path, 'r' ).readlines()
 warp_index = results[ -1 ].split( ' ' )[ -1 ]
 outf = open( args.output, 'wb' )
