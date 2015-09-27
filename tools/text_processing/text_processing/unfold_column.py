3044c93e0a940b21be18fe69a23522ce3a5476de
Björn Grüning <bjoern@gruenings.eu>
2015-09-27 16:58:24 +0200

    Update unfold_column.py

--- tools/text_processing/text_processing/unfold_column.py
+++ tools/text_processing/text_processing/unfold_column.py
@@ -4,11 +4,16 @@ import sys
 
 out = open(sys.argv[4], 'w+')
 
+sep = sys.argv[3]
+# un-sanitize Galaxy inputs
+if sep == 'X':
+    sep = ';'
+
 with open(sys.argv[1]) as handle:
     for line in handle:
         cols = line.split('\t')
         unfolding_column = int(sys.argv[2]) - 1
         column_content = cols[ unfolding_column ]
-        for elem in column_content.split( sys.argv[3] ):
+        for elem in column_content.split( sep ):
             out.write( '\t'.join( cols[:unfolding_column] + [elem] + cols[unfolding_column+1:]) )
 out.close()
