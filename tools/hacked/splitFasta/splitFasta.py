70d9ad4c354e3808a384532ba021b9fa5e101a98
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-10-16 21:52:11 +0200

    reimplement the entire wrapper to make it useful

--- tools/hacked/splitFasta/splitFasta.py
+++ tools/hacked/splitFasta/splitFasta.py
@@ -1,14 +1,13 @@
-import sys, os
+#!/usr/bin/env python
+import os
+import sys
+from Bio import SeqIO
+
 if __name__ == "__main__":
-    #assuming perfect input, read every two lines
     inpath = sys.argv[1]
-    file_contents = open(inpath, 'r').readlines()
-    os.makedirs('splits')
-    inname = os.path.basename(inpath)
-    for i in range(0, len(file_contents), 2):
-        headline = file_contents[i]
-        outname = headline[1:headline.index(' ')]+'.fa'
-        outfile = open(os.path.join('splits',outname), 'w')
-        outfile.write(file_contents[i])
-        outfile.write(file_contents[i+1])
-        outfile.close()
+    os.mkdir('splits')
+    with open(inpath, 'r') as handle:
+        for record in SeqIO.parse(handle, 'fasta'):
+            header = os.path.join('splits', record.id + '.fasta')
+            with open(header, 'w') as handle2:
+                SeqIO.write([record], handle2, 'fasta')
