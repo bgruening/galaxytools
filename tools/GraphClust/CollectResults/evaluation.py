f447414150c19865e904d3914a68e2479fadddce
Eteri Sokhoyan <esokhoyan@gmail.com>
2016-12-16 00:01:57 +0100

    Graphclust tools (#470)
    * GraphClust tools added
    
    * missing test-data added
    
    * missinf test-data added
    
    * missing test-data added
    
    * test-data for CollectResults fixed. (randomness removed)
    
    * CollectResults fixed.
    
    * comparison method for CollectResults chnaged.
    
    * test-case for CollectResults modified.
    
    * Milad's suggestions done.
    
    * test data for NSPDK fixed
    
    * file type for svecor changed to zip
    
    * mlocarana renamed
    
    * perl scripts removed, conda package for graphclust added
    
    * unnecessary test files removed.

---
+++ tools/GraphClust/CollectResults/evaluation.py
@@ -0,0 +1,51 @@
+import glob
+from os import system
+import re
+
+
+def sh(script):
+    system("bash -c '%s'" % script)
+
+
+dataNames = "FASTA/data.names"
+listOfClusters = []
+listOfClasses = []
+cluster_seqs_stats_path = "RESULTS/*.cluster.all"
+cluster_seqs_stats_files = glob.glob(cluster_seqs_stats_path)
+
+blackList = []
+numberOfClusters = 0
+for singleFile in sorted(cluster_seqs_stats_files):
+    numberOfClusters += 1
+    with open(singleFile, "r") as f:
+        for line in f.readlines():
+            uniqueId = line.split()[6]
+            clustNum = line.split()[1]
+            rnaClass, sep, tail = uniqueId.partition("_")
+            listOfClasses.append(rnaClass)
+            listOfClusters.append(clustNum)
+            with open(dataNames, "r") as names:
+                for line in names.readlines():
+                    fullUniqeId = line.split()[3]
+                    rnaClass, sep, tail = fullUniqeId.partition("_")
+                    short_unique = re.findall("_".join(["[^_]+"] * 2), fullUniqeId)[0]
+                    if short_unique == uniqueId:
+                        blackList.append(uniqueId)
+
+numberOfClusters += 1  # 1 cluster for all unassigned seqs
+with open(dataNames, "r") as names:
+    for line in names.readlines():
+        fullUniqeId = line.split()[3]
+        rnaClass, sep, tail = fullUniqeId.partition("_")
+        short_unique = re.findall("_".join(["[^_]+"] * 2), fullUniqeId)[0]
+        rnaClass, sep, tail = fullUniqeId.partition("_")
+        if short_unique not in blackList:
+            listOfClasses.append(rnaClass)
+            listOfClusters.append(str(numberOfClusters))
+            numberOfClusters += 1  # separate cluster for all unassigned seqs
+
+toWrite = ""
+for i in range(len(listOfClusters)):
+    toWrite += listOfClasses[i] + "\t" + listOfClusters[i] + '\n'
+with open("RESULTS/fullTab.tabular", "w") as full:
+    full.write(toWrite)
