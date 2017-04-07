833aad058388443c5d0a957c5869685d42e8481c
Dimitar-92 <dimitar.jovanov.92@gmail.com>
2017-04-07 16:45:20 +0200

    jsdom added and svg export functionality added

--- tools/fornac/server.js
+++ tools/fornac/server.js
@@ -1,6 +1,21 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var jsdom = require('jsdom');
+var fs = require("fs");
+var d3 = fs.readFileSync("././d3.js", "utf-8");
+var fornac = fs.readFileSync("./fornac.js", "utf-8");
 
-connect().use(serveStatic(__dirname)).listen(9000, function(){
-    console.log('Server running on 9000...');
-});
+var config = {};
+
+config.file = './editor.html';
+config.features = {
+	FetchExternalResources : ['script'],
+	ProcessExternalResources : ['script']
+};
+
+config.done = function (err, window) {
+  	// node service.js > test.svg to export
+  	// add styles to the styles element in the svg
+
+    console.log(window.$("#rna_ss").html());
+}
+
+jsdom.env(config);
