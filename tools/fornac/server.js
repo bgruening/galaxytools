fb5eddc0541137e4ac6ae9ba5e92e4f557ad1208
Dimitar-92 <dimitar.jovanov.92@gmail.com>
2017-04-10 15:32:11 +0200

    SVG export v2 - better style handling, displayeable in browser

--- tools/fornac/server.js
+++ tools/fornac/server.js
@@ -12,10 +12,21 @@ config.features = {
 };
 
 config.done = function (err, window) {
-  	// node service.js > test.svg to export
+  	// node server.js > test.svg to export
   	// add styles to the styles element in the svg
-
-    console.log(window.$("#rna_ss").html());
+  	var svgStyle = window.$("style").html();
+  	var svg = window.$("svg").attr("viewBox", "0 0 300 300");
+  	svg.attr("xmlns","http://www.w3.org/2000/svg");
+  	svg.attr("version","1.1");
+  	svg.attr("xmlns:xlink","http://www.w3.org/1999/xlink");
+  	window.$("svg > style").html(''+svgStyle);
+  	var svgFinal = 
+  		`<?xml version="1.0" standalone="no"?>
+<?xml-stylesheet type="text/css" href="style8.css"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+` +
+  		window.$("#rna_ss").html();
+    console.log(svgFinal);
 }
 
 jsdom.env(config);
