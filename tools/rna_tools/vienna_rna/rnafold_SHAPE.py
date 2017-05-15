7a3d3119bb8292f41c54fdb89d3a0df70c5844e1
Eteri <esokhoyan@gmail.com>
2017-05-15 16:59:14 +0200

    subprocess used instead of sh function.

--- tools/rna_tools/vienna_rna/rnafold_SHAPE.py
+++ tools/rna_tools/vienna_rna/rnafold_SHAPE.py
@@ -7,12 +7,9 @@ import sys
 from os import system
 from Bio import SeqIO
 import re
-
-def sh(script):
-    system("bash -c '%s'" % script)
+from subprocess import Popen, PIPE
 
 params_list = sys.argv[1:]
-
 param_list_no_shape = [s for s in params_list if not "--shape=" in s ]
 shape_file = [s for s in params_list if "--shape=" in s ]
 assert (len(shape_file) == 1)
@@ -27,9 +24,7 @@ id_line = ""
 with open(shape_file, 'r') as f:
     content = f.read()
     lines = content.split('\n')
-    for i in range(0, len(lines)):
-        line = lines[i]
-        #if pattern in line:
+    for line in lines:
         if pattern.match(line):
             id_line = line.split()[0]
             id_line = id_line[1:]
@@ -37,11 +32,14 @@ with open(shape_file, 'r') as f:
         else:
             with open(id_line +'.tmp', "a") as clFile:
                 clFile.write(line + "\n")
-
-
+                
 input_file = sys.stdin
 
 for record in SeqIO.parse(input_file, "fasta"):
     seq = ">{}\n{}".format(record.id,record.seq)
-    cmd =  'echo -e \"' + seq +'\" | RNAfold ' + "--shape=" + record.id + '.tmp ' + params_no_shape
-    sh(cmd)
+    cmd =  " RNAfold  --shape=" + record.id + '.tmp ' + params_no_shape
+    p = Popen(cmd , stdin=PIPE, shell=True, stdout=PIPE, stderr=PIPE)
+    out,err = p.communicate(seq.encode())
+    if err:
+        raise RuntimeError("Error in calling RNAfold\n{}\n{}\n".format(out, err))
+    print (out.decode('utf-8').strip())
