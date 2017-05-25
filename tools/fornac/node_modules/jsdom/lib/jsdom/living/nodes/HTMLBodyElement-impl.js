5e30ddff67301276fa6222d1be2996ef3a42d70e
Dimitar-92 <dimitar.jovanov.92@gmail.com>
2017-05-25 22:38:43 +0200

    JSDOM import changes and tool_dependencies.xml added

--- tools/fornac/node_modules/jsdom/lib/jsdom/living/nodes/HTMLBodyElement-impl.js
+++ tools/fornac/node_modules/jsdom/lib/jsdom/living/nodes/HTMLBodyElement-impl.js
@@ -1,25 +1,29 @@
 "use strict";
+
 const HTMLElementImpl = require("./HTMLElement-impl").implementation;
+
+const defineGetter = require("../../utils").defineGetter;
+const defineSetter = require("../../utils").defineSetter;
 const proxiedWindowEventHandlers = require("../helpers/proxied-window-event-handlers");
 
-class HTMLBodyElementImpl extends HTMLElementImpl {}
+class HTMLBodyElementImpl extends HTMLElementImpl {
 
-for (const name of proxiedWindowEventHandlers) {
-  Object.defineProperty(HTMLBodyElementImpl.prototype, name, {
-    configurable: true,
-    enumerable: true,
-    get() {
-      const window = this._ownerDocument._defaultView;
-      return window ? window[name] : null;
-    },
-    set(handler) {
+}
+
+(function () {
+  proxiedWindowEventHandlers.forEach(name => {
+    defineSetter(HTMLBodyElementImpl.prototype, name, function (handler) {
       const window = this._ownerDocument._defaultView;
       if (window) {
         window[name] = handler;
       }
-    }
+    });
+    defineGetter(HTMLBodyElementImpl.prototype, name, function () {
+      const window = this._ownerDocument._defaultView;
+      return window ? window[name] : null;
+    });
   });
-}
+}());
 
 module.exports = {
   implementation: HTMLBodyElementImpl
