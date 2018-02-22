b72419e40eee9925756529c43f2d979ceb006e43
Milad Miladi <miladim@cs.uni-freiburg.de>
2018-02-22 05:03:39 +0100

    evaluation custom path args

--- tools/GraphClust/CollectResults/evaluation.py
+++ tools/GraphClust/CollectResults/evaluation.py
@@ -4,18 +4,24 @@ from os import system
 import re
 from sklearn import metrics
 from shutil import make_archive
+import sys
+import fnmatch, os
 
 def sh(script):
     system("bash -c '%s'" % script)
 
-dataNames = "FASTA/data.names"
+fasta_dir = sys.argv[1]
+results_dir = sys.argv[2]
+dataNames = fasta_dir+"/data.names"
 
 listOfClusters = []
 listOfHeaders = []
 headersNames = set()
-cluster_seqs_stats_path = "RESULTS/*.cluster.all"
-cluster_seqs_stats_files = glob.glob(cluster_seqs_stats_path)
+idsNames = set()
 
+
+names = os.listdir(results_dir)
+cluster_seqs_stats_files = fnmatch.filter(names, '*.cluster.all')
 with open(dataNames, "r") as names:
     for line2 in names:
         splits2 = line2.split()
@@ -23,40 +29,52 @@ with open(dataNames, "r") as names:
         if len(splits2) >= 6:
             fullHeader = splits2[5]
             headersNames.add(fullHeader)
+            fullID = splits2[3]
+            idsNames.add(fullID)
 
 blackList = []
 numberOfClusters = 0
 for singleFile in sorted(cluster_seqs_stats_files):
+    singleFile = results_dir+'/'+singleFile
     numberOfClusters += 1
     with open(singleFile, "r") as f:
         for line in f:
             splits = line.split()
             header = ''
+            idd = ''
             if len(splits) >= 11:
                 header = splits[10]
+                idd = splits[8]
             clustNum = splits[2]
             listOfHeaders.append(header)
             listOfClusters.append(clustNum)
-            if header in headersNames:
-                blackList.append(header)
+            if idd in idsNames: #header in headersNames:
+                blackList.append(idd)
 
 numberOfClusters += 1  # 1 cluster for all unassigned seqs
+ignoreBlackList = False
 with open(dataNames, "r") as names:
     for line in names.readlines():
         splits = line.split() 
         fullUniqeId = splits[3]
         fullHeader = ''
+        fullID = ''
         if len(splits) >= 6:
             fullHeader = line.split()[5]
-        if fullHeader not in blackList or len(fullHeader) == 0:
+            fullID = line.split()[3]
+        if ignoreBlackList or ( fullID not in blackList #fullHeader not in blackList 
+            or len(fullHeader) == 0):
             listOfHeaders.append(fullHeader)
             listOfClusters.append(str(numberOfClusters))
             numberOfClusters += 1  # separate cluster for all unassigned seqs
+        # else:
+        #     print ("Skip header", fullHeader)
 
 toWrite = ""
 for i in range(len(listOfClusters)):
     toWrite += listOfHeaders[i] + "\t" + listOfClusters[i] + '\n'
-with open("RESULTS/fullTab.tabular", "w") as full:
+
+with open(results_dir+"/fullTab.tabular", "w") as full:
     full.write(toWrite)
 
 
@@ -72,10 +90,12 @@ if len(listOfHeaders) > 1: # and  pattern.match(str(listOfHeaders[0])):
 
     toWrite = "completeness_score : " + str(completeness_score) + "\n" + "homogeneity_score : " + str(homogeneity_score) + "\n" + "adjusted_rand_score : " +str(adjusted_rand_score)  + "\n" + "adjusted_mutual_info_score : " + str(adjusted_mutual_info_score)+ "\n" + "v_measure_score : " + str(v_measure_score)
 
+
 else:
     toWrite = "completeness_score : NA \nhomogeneity_score : NA \nadjusted_rand_score : NA \nadjusted_mutual_info_score : NA \nv_measure_score : NA"
 
-with open("RESULTS/evaluation.txt", "w") as fOut:
+print (toWrite)
+with open(results_dir+"/evaluation.txt", "w") as fOut:
     fOut.write(toWrite)
 
 
