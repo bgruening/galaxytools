78beeaf90531c24ffb03412df05fd201cef15316
Matthias Bernt <m.bernt@ufz.de>
2018-03-23 18:37:35 +0100

    bugfixes for gtf output

--- tools/rna_tools/rnacode/processMAF.sh
+++ tools/rna_tools/rnacode/processMAF.sh
@@ -35,6 +35,11 @@ key="$1"
 		args+=($1)
 		shift # past argument
 		;;
+		-g|--gtf)
+		gtf=1
+		args+=($1)
+		shift # past argument
+		;;
 		-t|--tabular)
 		tabular=1
 		args+=($1)
@@ -45,13 +50,14 @@ key="$1"
 		shift # past argument
 		shift # past value
 		;;
-		-g|--gtf|-b|--best-only|-r|--best-region|-s|--stop-early)
+		-b|--best-only|-r|--best-region|-s|--stop-early)
 		args+=($1)
 		shift # past argument
 		;;
 		-n|--num-samples|-p|--cutoff|-c|--pars|-i|--eps-cutoff)
 		args+=($1 $2)
 		shift # past argument
+		shift # past value
 		;;
 		-h|--help|-v|--version)
 		shift # past argument
@@ -71,9 +77,18 @@ function fix_output {
  	fi
 	while read line
  	do
- 		i=`echo "$line" | awk '{print $1}'`
+		if [[ -z "$gtf" ]]; then
+			i=`echo "$line" | sed 's/^\([[:digit:]]\+\)[[:space:]].*/\1/'`
+		else
+			i=`echo $line | sed 's/.*Gene\([[:digit:]]\+\).*/\1/'`
+		fi
  		j=`echo "$i+$last" | bc`
-		echo $line | awk -v n=$j '{printf("%d\t", n); for(i=2; i<=NF; i++){printf("%s", $(i)); if(i==NF){printf("\n")}else{printf("\t")}}}'
+		if [[ -z "$gtf" ]]; then
+			echo "$line" | sed "s/^\([[:digit:]]\+\)\([[:space:]].*\)/$j\2/"
+		else
+			echo "$line" | sed "s/^\(.*\)Gene[0-9]\+\(\".*\)$/\1Gene$j\2/"
+		fi
+		#echo $line | awk -v n=$j '{printf("%d\t", n); for(i=2; i<=NF; i++){printf("%s", $(i)); if(i==NF){printf("\n")}else{printf("\t")}}}'
 		if [[ ! -z "$eps" && -f ${epsdir:-eps}/hss-$i.eps ]]; then
 			mv ${epsdir:-eps}/hss-$i.eps $tmpd/hss-$j.eps
 		fi
@@ -83,26 +98,32 @@ function fix_output {
  		unset j
  	fi
 }
- 
+
 # run RNAcode for $tempfile if >= 3 sequences
 function run_rnacode {
-        >&2 echo -e "processing" `cat ${tmpif} | grep $ref | cut -d" " -f1-6`
-	nl=`cat ${tmpif} | grep "^s" | wc -l`
+	>&2 echo -e "processing " `cat ${tmpif} | grep ^s | head -n 1 | cut -d" " -f1-6`
+# 	>&2 echo "with RNAcode" $@
+ 	nl=`cat ${tmpif} | grep "^s" | wc -l`
 	if [[ "$nl" -ge "3" ]]; then
-		streams=$(mktemp -p '.')
-		RNAcode $@ &> $streams
+		# - filter the outfile for lines containing the ref and redirect everything to stderr
+		#   https://github.com/wash/rnacode/issues/9
+		# - we can not pipe stdout | ... | fix_output since then $last can not be used as global variable
+		if [[ ! -z "$gtf" ]]; then
+			field=1
+		elif [[ ! -z "$tabular" ]]; then
+			field=7
+		else
+			field=6
+		fi
+		RNAcode $@ | awk -v ref=$ref -v field=$field '{if($(field)==ref){print $0}else{$0 > "/dev/stderr"}}' > ${tmpof} 
+
 		if [[ "$?" != "0" ]]; then
 			ef=$(mktemp -u -p '.')
 			cat ${tmpif} > ${ef}.maf
 			>&2 echo "RNAcode failed for the alignmentblock \""`cat ${tmpif} | grep $ref | cut -d" " -f 1-6`"\" (${ef}.maf)"
 		fi
-		# - filter stdout for lines containing the ref and redirect everything to stderr
-		#   https://github.com/wash/rnacode/issues/9
-		# - we can not pipe stdout | ... | fix_output since then $last can not be used as global variable
-		cat $streams | grep -v $ref | egrep -v "^ HSS #|^====|^$" 1>&2 
-		sed -i -n "/$ref/p" $streams
-		fix_output < $streams 
-		rm $streams
+		fix_output < $tmpof
+		echo -n > ${tmpof}
 	else
 		>&2 echo "less than 3 sequences in the alignment block \""`cat ${tmpif} | grep $ref | cut -d" " -f 1-6`"\""
 	fi
@@ -112,11 +133,7 @@ ref=""
 last=0
 
 if [[ ! -z "$tabular" ]]; then
-	if [[ ! -z "$outfile"  ]]; then
-		echo -e "HSS #\tFrame\tLength\tFrom\tTo\tName\tStart\tEnd\tScore\tP" > "$outfile"
-	else
-		echo -e "HSS #\tFrame\tLength\tFrom\tTo\tName\tStart\tEnd\tScore\tP"
-	fi
+	echo -e "HSS #\tFrame\tLength\tFrom\tTo\tName\tStart\tEnd\tScore\tP" >> ${outfile:-/dev/stdout}
 fi
 
 tmpif=$(mktemp -p '.')
@@ -128,16 +145,22 @@ tmpd=$(mktemp -d -p '.')
 # - empty lines: process tmpif (ie. last alignment block) with RNAcode, clear tmpif
 # - on the go the name of the reference species is determined from the 1st line 
 #   of the alignment this is used then for filtering the RNAcode output
+#   in case of gtf output only the chromosome is printed, ie only chr1 instead of dm6.chr1 
 while read line
 do
 	if [[ "$line" =~ ^# ]]; then
 		echo -n > ${tmpif}
 	elif [[ "$line" =~ ^$ ]]; then
 		run_rnacode ${args[@]} ${tmpif}
+		# >> ${outfile:-/dev/stdout}
 		echo -n > ${tmpif}
 	else
                 if [[ -z $ref && "$line" =~ ^s ]]; then
-			ref=`echo $line | sed 's/\./ /g' | cut -d" " -f 2`
+			if [[ -z "$gtf" ]]; then
+				ref=`echo $line | cut -d" " -f 2`
+			else
+				ref=`echo $line | sed 's/\./ /g' | cut -d" " -f 3`
+			fi
 		fi
 		echo $line >> ${tmpif}
 	fi
@@ -145,10 +168,7 @@ done < ${file:-/dev/stdin}
 # if there is something left -> process it
 if [[ "`cat ${tmpif} | wc -l`" -gt "0" ]]; then
 	run_rnacode ${args[@]} ${tmpif}
-fi
-
-if [[ ! -z "$outfile" ]]; then
-	cat $tmpof > "$outfile"
+       #	>> ${outfile:-/dev/stdout} 
 fi
 
 if [[ ! -z "$eps" ]]; then
