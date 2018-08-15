d1b4459d030745272e47111b5d67aca17cdcbba7
Jelle Scholtalbers <j.scholtalbers@gmail.com>
2018-08-15 13:16:11 +0200

    update bismark to 0.19.1 and cleanup wrappers a bit

--- tools/bismark/bismark_deduplicate/bismark_deduplicate_wrapper.py
+++ tools/bismark/bismark_deduplicate/bismark_deduplicate_wrapper.py
@@ -1,65 +1,79 @@
 #!/usr/bin/python
 
 import argparse
+import logging
 import os
-import re
 import shutil
 import subprocess
 import sys
 import tempfile
-import logging
 from glob import glob
+
 
 def cleanup_before_exit(tmp_dir):
     if tmp_dir and os.path.exists(tmp_dir):
         shutil.rmtree(tmp_dir)
 
+
+def stop_err(logger, msg):
+    logger.critical(msg)
+    sys.exit(1)
+
+
+def log_subprocess_output(logger, pipe):
+    for line in iter(pipe.readline, b''):
+        logger.debug(line.decode().rstrip())
+
+
 def get_arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tool_dir', dest='tool_dir', action='store', nargs=1, metavar='tool_dir', type=str)
     parser.add_argument('-p', action='store_true')
     parser.add_argument('-s', action='store_true')
-    parser.add_argument('--input', dest='input', action='store', nargs=1, metavar='input', type=str)
-    parser.add_argument('--output_report', dest='output_report', action='store', nargs=1, metavar='output_report', type=str)
-    parser.add_argument('--output_bam', dest='output_bam', action='store', nargs=1, metavar='output_report', type=str)
-    parser.add_argument('--log_report', dest='log_report', action='store', nargs=1, metavar='log_filename', type=str)
+    parser.add_argument('--input', dest='input', metavar='input')
+    parser.add_argument('--output_report', dest='output_report', metavar='output_report')
+    parser.add_argument('--output_bam', dest='output_bam', metavar='output_report')
+    parser.add_argument('--log_report', dest='log_report', metavar='log_filename', type=str)
     args = parser.parse_args()
     return args
+
 
 def __main__():
     args = get_arg()
 
+    logger = logging.getLogger('bismark_deduplicate_wrapper')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    if args.log_report:
+        ch.setLevel(logging.WARNING)
+        handler = logging.FileHandler(args.log_report)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+    else:
+        ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
     tmp_dir = tempfile.mkdtemp(prefix='tmp', suffix='')
     os.chdir(tmp_dir)
 
-    if args.log_report:
-        logging.basicConfig(level=logging.INFO, filename=args.log_report[0], filemode="a+", format='%(message)s')
-    else:
-        logging.basicConfig(level=logging.INFO, filename=os.path.join(tmp_dir, 'log_report.txt'), filemode="a+", format='%(message)s')
-
     default_reads_name = 'submitted_reads.bam'
-    os.symlink(args.input[0], default_reads_name)
+    os.symlink(args.input, default_reads_name)
 
     if args.p is True:
         sPaired = '-p'
     if args.s is True:
         sPaired = '-s'
 
-    cmd = 'perl %s %s duplicated_reads.bam --bam' % (os.path.join(args.tool_dir[0], 'deduplicate_bismark'), sPaired)
-    logging.info('COMMAND LINE:\n\n%s' % cmd)
+    cmd = ['deduplicate_bismark', sPaired, default_reads_name, '--bam']
+    logger.info("Deduplicating with: '%s'", " ".join(cmd))
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    with process.stdout:
+        log_subprocess_output(logger, process.stdout)
+    exitcode = process.wait()
+    if exitcode != 0:
+        stop_err(logger, "Bismark deduplication error (also check the log file if any):\n%s" % process.stderr)
 
-    proc = subprocess.Popen(['perl', os.path.join(args.tool_dir[0], 'deduplicate_bismark'), sPaired, default_reads_name, '--bam'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    proc_out, proc_err = proc.communicate()
-
-    logging.info("__________________________________________________________________\n")
-    logging.info("BISMARK DEDUPLICATE STDOUT:\n\n%s" % proc_out)
-    if proc_err:
-        logging.critical("__________________________________________________________________\n")
-        logging.critical("BISMARK DEDUPLICATE WARNING:\n\n%s" % proc_err)
-        sys.exit("Dedpulicate Bismark crashed with the folowing error message:\n%s" % proc_err)
-
-    shutil.move( glob('*deduplicated.bam')[0], args.output_bam[0] )
-    shutil.move( glob('*deduplication_report.txt')[0], args.output_report[0])
+    shutil.move( glob('*deduplicated.bam')[0], args.output_bam )
+    shutil.move( glob('*deduplication_report.txt')[0], args.output_report)
 
     cleanup_before_exit(tmp_dir)
 
