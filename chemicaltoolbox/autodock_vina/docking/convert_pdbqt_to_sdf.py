64da23215f7ff4ca5c0e74231ed1326c7dda44fb
Simon Bray <sbray1371@gmail.com>
2019-06-18 12:01:37 +0200

    cosmetic changes to python script

--- chemicaltoolbox/autodock_vina/docking/convert_pdbqt_to_sdf.py
+++ chemicaltoolbox/autodock_vina/docking/convert_pdbqt_to_sdf.py
@@ -9,7 +9,7 @@ def main():
 		print("Usage: convert_pdbqt_to_sdf.py <input-pdbqt-file> <output-sdf-file>")
 		exit(1)
 
-def AddProperty(mol, prop_name, prop_value):
+def add_property(mol, prop_name, prop_value):
 	newData = openbabel.OBPairData() 
 	newData.SetAttribute(prop_name)
 	newData.SetValue(prop_value) 
@@ -20,17 +20,16 @@ def process(input, output):
 	sdf = pybel.Outputfile("sdf", output, overwrite=True)
 	for mol in docked:
 		if mol.OBMol.HasData('REMARK'):
-
 			remark = mol.OBMol.GetData('REMARK').GetValue()
 			lines = remark.splitlines()
 			tokens = lines[0].split()
 		
 			# add the score property
-			AddProperty(mol, "SCORE", tokens[2]) 
+			add_property(mol, "SCORE", tokens[2]) 
 			# add the first RMSD property
-			AddProperty(mol, "RMSD_LB", tokens[3])
-			# add the decond RMSD property
-			AddProperty(mol, "RMSD_UB", tokens[4])
+			add_property(mol, "RMSD_LB", tokens[3])
+			# add the second RMSD property
+			add_property(mol, "RMSD_UB", tokens[4])
 
 		sdf.write(mol)
 
