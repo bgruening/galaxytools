948789f32f275dbe56f5c9c0eec3d04468d5d2d9
Qiang Gu <guqiang01@gmail.com>
2019-09-10 12:54:23 -0700

    add help and param checker for save best estimator

--- tools/sklearn/search_model_validation.py
+++ tools/sklearn/search_model_validation.py
@@ -213,6 +213,16 @@ def main(inputs, infile_estimator, infile1, infile2,
     with open(inputs, 'r') as param_handler:
         params = json.load(param_handler)
 
+    # conflict param checker
+    if params['outer_split']['split_mode'] == 'nested_cv' \
+            and params['save'] != 'nope':
+        raise ValueError("Save best estimator is not possible for nested CV!")
+
+    if not (params['search_schemes']['options']['refit']) \
+            and params['save'] != 'nope':
+        raise ValueError("Save best estimator is not possible when refit "
+                         "is False!")
+
     params_builder = params['search_schemes']['search_params_builder']
 
     with open(infile_estimator, 'rb') as estimator_handler:
