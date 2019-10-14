2adc3216d745bf502bf3b9180ab33cd08b9b59ad
Alexander OSTROVSKY <alexanderostrovsky@Caroline.local>
2019-10-14 17:09:12 -0400

    alevin updated with vpolo, convert umi graphs to pdfs

--- tools/salmon/vpolo_convert.py
+++ tools/salmon/vpolo_convert.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-from vpolo.alevin import parser
-#import argparse
+from vpolo.alevin import parser as par
+import argparse
+import os
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("--mtx", "-m", action="store_true", help="--dumpMtx flag set")
-#parser.add_argument("--umi", "-u", action="store_true", help="--dumpUmiGraph flag set")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--mtx", "-m", action="store_true", help="--dumpMtx flag set")
+parser.add_argument("--umi", "-u", action="store_true", help="--dumpUmiGraph flag set")
+args = parser.parse_args()
 
 if args.mtx:
-    alevin_df = parser.read_quants_bin("output")
+    alevin_df = par.read_quants_bin("output")
     with open("quants_mat.tsv", "w") as f:
         f.write(alevin_df.to_csv(sep="\t"))
 
-#if args.umi:
-#    parser.read_umi_graph("output", "umigraph")
+if args.umi:
+    os.mkdir("umiout")
+    par.read_umi_graph("output", "umiout")
