8c52d034304c07d640603a9f3b92527f6f675c4b
Simon Bray <32272674+simonbray@users.noreply.github.com>
2020-04-03 12:37:31 +0200

    [skip ci]

--- chemicaltoolbox/rdock/rbdock.py
+++ chemicaltoolbox/rdock/rbdock.py
@@ -1,23 +1,29 @@
 import subprocess
-import sys
-import os
+import argparse
 
-try:
-    int(sys.argv[1])
-    num = sys.argv[1]
-except (ValueError, IndexError) as e:
-    exit(1)
+def main():
+    parser = argparse.ArgumentParser(description='Simple wrapper for rbdock')
+    parser.add_argument('-n', '--num', type=int, help='Equilibrated system as input')
+    parser.add_argument('-s', '--seed', type=int, help='Random seed')
+    args = parser.parse_args()
 
-ps = subprocess.Popen(['rbdock', '-i', 'ligands.sdf', '-r', 'receptor.prm', '-p', 'dock.prm', '-n', str(num), '-o', 'output'], stdout=subprocess.PIPE)
+    cmd = ['rbdock', '-i', 'ligands.sdf', '-r', 'receptor.prm', '-p', 'dock.prm', '-n', str(args.num), '-o', 'rdock_output']
+    if args.seed != None:
+        cmd += ['-s', str(args.seed)]
 
-error_counter = 0
-for stdout_line in iter(ps.stdout.readline, ''):
-    print(stdout_line)
-    if 'RBT_DOCKING_ERROR' in str(stdout_line):
-            error_counter += 1
-            if error_counter == 10:
-                print(ps.stdout)
-                exit(23)
-    if ps.poll() != None:
-        print(ps.stdout)
-        exit(int(ps.poll()))
+    ps = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    error_counter = 0
+    for stdout_line in iter(ps.stdout.readline, ''):
+        print(stdout_line)
+        if 'RBT_DOCKING_ERROR' in str(stdout_line):
+                error_counter += 1
+                if error_counter == 10:
+                    print(ps.stdout)
+                    exit(23)
+        if ps.poll() != None:
+            print(ps.stdout)
+            exit(int(ps.poll()))
+
+if __name__ == "__main__":
+    main()
