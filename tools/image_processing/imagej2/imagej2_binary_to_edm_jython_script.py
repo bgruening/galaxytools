a3349936052cfc0d0e9a16c6d9cb2fd56dab4dc9
greg <ghv2@psu.edu>
2020-09-07 13:42:04 -0400

    Batch 4: some output differ, possibly due to previous tests on osx, current on linux.

--- tools/image_processing/imagej2/imagej2_binary_to_edm_jython_script.py
+++ tools/image_processing/imagej2/imagej2_binary_to_edm_jython_script.py
@@ -1,44 +1,40 @@
-import jython_utils
 import sys
+
 from ij import IJ
-from ij import ImagePlus
-from ij.plugin.filter import Analyzer
-from ij.plugin.filter import EDM
 
 # Fiji Jython interpreter implements Python 2.5 which does not
 # provide support for argparse.
-error_log = sys.argv[ -8 ]
-input = sys.argv[ -7 ]
-iterations = int( sys.argv[ -6 ] )
-count = int( sys.argv[ -5 ] )
-black_background = jython_utils.asbool( sys.argv[ -4 ] )
-pad_edges_when_eroding = jython_utils.asbool( sys.argv[ -3 ] )
-tmp_output_path = sys.argv[ -2 ]
-output_datatype = sys.argv[ -1 ]
+error_log = sys.argv[-8]
+input_file = sys.argv[-7]
+iterations = int(sys.argv[-6])
+count = int(sys.argv[-5])
+black_background = sys.argv[-4] == "yes"
+pad_edges_when_eroding = sys.argv[-3] == "yes"
+output_filename = sys.argv[-2]
+output_datatype = sys.argv[-1]
 
 # Open the input image file.
-input_image_plus = IJ.openImage( input )
+input_image_plus = IJ.openImage(input_file)
 
 # Create a copy of the image.
 input_image_plus_copy = input_image_plus.duplicate()
 image_processor_copy = input_image_plus_copy.getProcessor()
 
-try:
-    # Set binary options.
-    options = jython_utils.get_binary_options( black_background=black_background,
-                                               iterations=iterations,
-                                               count=count,
-                                               pad_edges_when_eroding=pad_edges_when_eroding )
-    IJ.run( input_image_plus_copy, "Options...", options )
+# Set binary options.
+options_list = ['edm=Overwrite', 'iterations=%d' % iterations, 'count=%d' % count]
+if black_background:
+    options_list.append("black")
+if pad_edges_when_eroding:
+    options_list.append("pad")
+options = " ".join(options_list)
+IJ.run(input_image_plus_copy, "Options...", options)
 
-    # Convert image to binary if necessary.
-    if not image_processor_copy.isBinary():
-        # Convert the image to binary grayscale.
-        IJ.run( input_image_plus_copy, "Make Binary", "" )
+# Convert image to binary if necessary.
+if not image_processor_copy.isBinary():
+    # Convert the image to binary grayscale.
+    IJ.run(input_image_plus_copy, "Make Binary", "")
 
-    # Run the command.
-    IJ.run( input_image_plus_copy, "Distance Map", "" )
-    # Save the ImagePlus object as a new image.
-    IJ.saveAs( input_image_plus_copy, output_datatype, tmp_output_path )
-except Exception, e:
-    jython_utils.handle_error( error_log, str( e ) )
+# Run the command.
+IJ.run(input_image_plus_copy, "Distance Map", "")
+# Save the ImagePlus object as a new image.
+IJ.saveAs(input_image_plus_copy, output_datatype, output_filename)
--- tools/image_processing/imagej2/imagej2_bunwarpj_align_jython_script.py
+++ tools/image_processing/imagej2/imagej2_bunwarpj_align_jython_script.py
@@ -1,37 +1,59 @@
+import os
 import sys
-import jython_utils
+import tempfile
+
 from ij import IJ
+
+
+def convert_before_saving_as_tiff(image_plus):
+    # The bUnwarpJ plug-in produces TIFF image stacks consisting of 3
+    # slices which can be viewed in ImageJ.  The 3 slices are: 1) the
+    # registered image, 2) the target image and 3) the black/white warp
+    # image.  When running bUnwarpJ from the command line (as these
+    # Galaxy wrappers do) the initial call to IJ.openImage() (to open the
+    # registered source and target images produced by bUnwarpJ) in the
+    # tool's jython_script.py returns an ImagePlus object with a single
+    # slice which is the "generally undesired" slice 3 discussed above.
+    # However, a call to IJ.saveAs() will convert the single-slice TIFF
+    # into a 3-slice TIFF image stack (as described above) if the selected
+    # format for saving is TIFF.  Galaxy supports only single-layered
+    # images, so to work around this behavior, we have to convert the
+    # image to something other than TIFF so that slices are eliminated.
+    # We can then convert back to TIFF for saving.  There might be a way
+    # to do this without converting twice, but unknown.
+    tmp_dir = tempfile.mkdtemp(prefix='tmp-imagej-')
+    fd, name = tempfile.mkstemp(suffix='png', dir=tmp_dir)
+    os.close(fd)
+    IJ.saveAs(image_plus, 'png', name)
+    return IJ.openImage(name)
+
 
 # Fiji Jython interpreter implements Python 2.5 which does not
 # provide support for argparse.
-
-if sys.argv[ -1 ].lower() in [ 'true' ]:
-    mono = True
-else:
-    mono = False
+mono = sys.argv[-1] == "true"
 
 if mono:
     # bUnwarpJ has been called with the -mono param.
-    source_tiff_path = sys.argv[ -4 ]
-    source_datatype = sys.argv[ -3 ]
-    source_path = sys.argv[ -2 ]
+    source_tiff_path = sys.argv[-4]
+    source_datatype = sys.argv[-3]
+    source_path = sys.argv[-2]
 else:
-    source_tiff_path = sys.argv[ -7 ]
-    source_datatype = sys.argv[ -6 ]
-    source_path = sys.argv[ -5 ]
-    target_tiff_path = sys.argv[ -4 ]
-    target_datatype = sys.argv[ -3 ]
-    target_path = sys.argv[ -2 ]
+    source_tiff_path = sys.argv[-7]
+    source_datatype = sys.argv[-6]
+    source_path = sys.argv[-5]
+    target_tiff_path = sys.argv[-4]
+    target_datatype = sys.argv[-3]
+    target_path = sys.argv[-2]
 
 # Save the Registered Source Image.
-registered_source_image = IJ.openImage( source_tiff_path )
+registered_source_image = IJ.openImage(source_tiff_path)
 if source_datatype == 'tiff':
-    registered_source_image = jython_utils.convert_before_saving_as_tiff( registered_source_image )
-IJ.saveAs( registered_source_image, source_datatype, source_path )
+    registered_source_image = convert_before_saving_as_tiff(registered_source_image)
+IJ.saveAs(registered_source_image, source_datatype, source_path)
 
 if not mono:
     # Save the Registered Target Image.
-    registered_target_image = IJ.openImage( target_tiff_path )
+    registered_target_image = IJ.openImage(target_tiff_path)
     if target_datatype == 'tiff':
-        registered_target_image = jython_utils.convert_before_saving_as_tiff( registered_target_image )
-    IJ.saveAs( registered_target_image, target_datatype, target_path )
+        registered_target_image = convert_before_saving_as_tiff(registered_target_image)
+    IJ.saveAs(registered_target_image, target_datatype, target_path)
