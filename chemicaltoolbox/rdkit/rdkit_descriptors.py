9c0d22b1c5349334ff985212b9ff08170abcca61
Simon Bray <sbray1371@gmail.com>
2021-02-17 12:36:11 +0100

    fix

--- chemicaltoolbox/rdkit/rdkit_descriptors.py
+++ chemicaltoolbox/rdkit/rdkit_descriptors.py
@@ -6,7 +6,25 @@ import sys
 
 from rdkit import Chem
 from rdkit.Chem import Descriptors
-from rdkit_util import get_supplier
+
+
+def get_supplier(infile, format='smiles'):
+    """
+    Returns a generator over a SMILES or InChI file. Every element is of RDKit
+    molecule and has its original string as _Name property.
+    """
+    with open(infile) as handle:
+        for line in handle:
+            line = line.strip()
+            if format == 'smiles':
+                mol = Chem.MolFromSmiles(line, sanitize=True)
+            elif format == 'inchi':
+                mol = Chem.inchi.MolFromInchi(line, sanitize=True, removeHs=True, logLevel=None, treatWarningAsError=False)
+            if mol is None:
+                yield False
+            else:
+                mol.SetProp('_Name', line.split('\t')[0])
+                yield mol
 
 
 def get_rdkit_descriptor_functions():
