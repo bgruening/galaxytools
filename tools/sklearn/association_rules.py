c4cfaa62f195d2e2f1704ff527e93453ccfacaf3
Kaivan Kamali <kxk302@psu.edu>
2021-04-20 10:31:42 -0400

    Cleaned up MBA code after self review.

--- tools/sklearn/association_rules.py
+++ tools/sklearn/association_rules.py
@@ -56,38 +56,48 @@ def main(inputs, infile, outfile, min_support=0.5, min_confidence=0.5, min_lift=
       line_items = line.split("\t")
       dataset.append(line_items)
   
+    # TransactionEncoder learns the unique labels in the dataset and transforms the
+    # input dataset (a Python list of lists) into a one-hot encoded NumPy boolean array
     te = TransactionEncoder()
     te_ary = te.fit_transform(dataset)
 
+    # Turn the encoded NumPy array into a DataFrame
     df = pd.DataFrame(te_ary, columns=te.columns_)
 
+    # Extract frequent itemsets for association rule mining
+    # use_colnames: Use DataFrames' column names in the returned DataFrame instead of column indices
     frequent_itemsets = fpgrowth(df, min_support=min_support, use_colnames=True, max_len=max_length)
 
-    # Get association rules, with lift larger than min_lift  
+    # Get association rules, with confidence larger than min_confidence
     rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=min_confidence)
 
-    # Filter association rules, keeping rules with lift and conviction larger than min_liftand min_conviction
+    # Filter association rules, keeping rules with lift and conviction larger than min_liftand and min_conviction
     rules = rules[ (rules['lift'] >= min_lift) & (rules['conviction'] >= min_conviction) ]
 
     # Convert columns from frozenset to list (more readable)
     rules['antecedents'] = rules['antecedents'].apply(list)
     rules['consequents'] = rules['consequents'].apply(list)
 
-    # Sort items in columns
+    # The next 3 steps are intended to fix the order of the association
+    # rules generated, so tests that rely on diff'ing a desired output
+    # with an expected output can pass.  
+
+    # 1) Sort entry in every row/column for columns 'antecedents' and 'consequents'
     rules['antecedents'] = rules['antecedents'].apply(lambda row: sorted(row))
     rules['consequents'] = rules['consequents'].apply(lambda row: sorted(row))
 
-    # Create two temporary string columns to srot on
+    # 2) Create two temporary string columns to sort on
     rules['ant_str'] = rules['antecedents'].apply(lambda row: " ".join(row))
     rules['con_str'] = rules['consequents'].apply(lambda row: " ".join(row))
 
-    # Sort results so they are re-producable
+    # 3) Sort results so they are re-producable
     rules.sort_values(by=['ant_str', 'con_str'], inplace=True)
     del rules['ant_str']
     del rules['con_str']
     rules.reset_index(drop=True, inplace=True)
 
-    rules.to_csv(outfile, sep="\t")
+    # Write association rules and metrics to file
+    rules.to_csv(outfile, sep="\t", index=False)
 
 
 if __name__ == '__main__':
