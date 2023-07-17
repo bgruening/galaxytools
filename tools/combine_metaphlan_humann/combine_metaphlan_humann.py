4d57442f395223cfd53b26701598f394b14d04fd
Ubuntu <19364610+tuncK@users.noreply.github.com>
2023-07-17 11:10:56 +0000

    Updated to new metaphlan version

--- tools/combine_metaphlan_humann/combine_metaphlan_humann.py
+++ tools/combine_metaphlan_humann/combine_metaphlan_humann.py
@@ -4,16 +4,27 @@
 import argparse
 
 
-def extract_clade_abundance(metaphlan2_fp):
+def extract_clade_abundance(metaphlan_fp):
     clade_abund = {}
-    with open(metaphlan2_fp, "r") as metaphlan2_f:
-        for line in metaphlan2_f.readlines():
+    with open(metaphlan_fp, "r") as metaphlan_f:
+        is_metaphlan_v4 = False
+        for line in metaphlan_f.readlines():
+            if 'SGB' in line:
+                # New versions of metaphlan against a recent DB contain a header line with DB name, which contains SGB
+                is_metaphlan_v4 = True
             if line.find("g__") == -1:
                 continue
 
             split_line = line[:-1].split("\t")
             taxo = split_line[0]
-            abundance = split_line[1]
+            if is_metaphlan_v4:
+                # Column order in new metaphlan versions:
+                # clade_name NCBI_tax_id relative_abundance additional_species
+                abundance = split_line[2]
+            else:
+                # Column order in the old metaphlan versions:
+                # clade_name relative_abundance coverage average_genome_length_in_the_clade estimated_number_of_reads_from_the_clade
+                abundance = split_line[1]
 
             genus = taxo[(taxo.find("g__") + 3):]
             if genus.find("|") != -1:
@@ -29,10 +40,10 @@ def extract_clade_abundance(metaphlan2_fp):
     return clade_abund
 
 
-def compute_overall_abundance(humann2_fp):
+def compute_overall_abundance(humann_fp):
     overall_abundance = 0
-    with open(humann2_fp, "r") as humann2_f:
-        for line in humann2_f.readlines():
+    with open(humann_fp, "r") as humann_f:
+        for line in humann_f.readlines():
             if line.find("|") != -1 or line.startswith("#"):
                 continue
             split_line = line[:-1].split("\t")
@@ -52,16 +63,16 @@ def format_characteristic_name(name):
     return formatted_n
 
 
-def combine_metaphlan2_humann2(args):
-    clade_abund = extract_clade_abundance(args.metaphlan2_fp)
-    overall_abund = compute_overall_abundance(args.humann2_fp)
+def combine_metaphlan_humann(args):
+    clade_abund = extract_clade_abundance(args.metaphlan_fp)
+    overall_abund = compute_overall_abundance(args.humann_fp)
 
     with open(args.output_fp, "w") as output_f:
         s = "genus\tgenus_abundance\tspecies\tspecies_abundance\t"
         s = "%s\t%s_id\t%s_name\t%s_abundance\n" % (s, args.type, args.type, args.type)
         output_f.write(s)
-        with open(args.humann2_fp, "r") as humann2_f:
-            for line in humann2_f.readlines():
+        with open(args.humann_fp, "r") as humann_f:
+            for line in humann_f.readlines():
                 if line.find("|") == -1:
                     continue
 
@@ -81,12 +92,12 @@ def combine_metaphlan2_humann2(args):
                 species = taxo[1][3:]
 
                 if genus not in clade_abund:
-                    print("no %s found in %s" % (genus, args.metaphlan2_fp))
+                    print("no %s found in %s" % (genus, args.metaphlan_fp))
                     continue
                 if species not in clade_abund[genus]["species"]:
                     print(
-                        "no %s found in %s for % s"
-                        % (species, args.metaphlan2_fp, genus)
+                        "No %s found in %s for % s"
+                        % (species, args.metaphlan_fp, genus)
                     )
                     continue
 
@@ -98,10 +109,10 @@ def combine_metaphlan2_humann2(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--humann2_fp", required=True)
-    parser.add_argument("--metaphlan2_fp", required=True)
+    parser.add_argument("--humann_fp", required=True)
+    parser.add_argument("--metaphlan_fp", required=True)
     parser.add_argument("--output_fp", required=True)
     parser.add_argument("--type", required=True, choices=["gene_families", "pathways"])
     args = parser.parse_args()
 
-    combine_metaphlan2_humann2(args)
+    combine_metaphlan_humann(args)
