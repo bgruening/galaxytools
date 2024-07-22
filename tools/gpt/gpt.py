c1cee464941ed26462997899421a35f0d099152e
Arash <arash77.kad@gmail.com>
2024-07-22 16:23:25 +0200

    update tool

--- tools/gpt/gpt.py
+++ tools/gpt/gpt.py
@@ -4,10 +4,12 @@ from openai import OpenAI
 
 context = sys.argv[1]
 question = sys.argv[2]
-client = OpenAI(api_key="")
+model = sys.argv[3]
+openai_api_key = sys.argv[4]
+client = OpenAI(api_key=openai_api_key)
 
 response = client.chat.completions.create(
-    model="gpt-4o",
+    model=model,
     messages=[
         {
             "role": "system",
