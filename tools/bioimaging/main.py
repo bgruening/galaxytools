73f6a41b4fdff2d58b4c51db7c3d5c0f71fe6f41
anuprulez <anup.rulez@gmail.com>
2024-07-31 19:33:16 +0200

    fix dymanic input shapes

--- tools/bioimaging/main.py
+++ tools/bioimaging/main.py
@@ -19,6 +19,19 @@ from numpy.typing import NDArray
 from typing_extensions import assert_never
 
 
+def find_dim_order(user_in_shape, input_image):    
+    image_shape = list(input_image.shape)
+    print(user_in_shape, image_shape)
+    correct_order = user_in_shape.split(",")[::-1]
+    print("correct_order", correct_order)
+    correct_order = [int(i) for i in correct_order if i != "1"]
+    print("correct order:", correct_order)
+    if (correct_order[0] == image_shape[-1]) and (correct_order != image_shape):
+        input_image = torch.tensor(input_image.transpose())
+    return input_image, correct_order
+
+
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-im", "--imaging_model", required=True, help="Input BioImage model")
@@ -30,67 +43,74 @@ if __name__ == "__main__":
     model_path = args["imaging_model"]
     input_image_path = args["image_file"]
     test_data = imageio.v3.imread(input_image_path, index="...")
-    #model = torch.load(model_path)
-    #model.eval()
+    test_data = np.squeeze(test_data)
+    test_data = test_data.astype(np.float32)
+
     test_data_mat = np.load(args["image_file_npy"])
     test_data_mat = torch.Tensor(test_data_mat)
     print("test data mat:", test_data_mat.shape)
     input_image_shape = args["image_size"]
-    print(test_file_size)
-    target_shape = input_image_shape.split(",")
-    target_shape = [int(i) for i in target_shape]
-    print(target_shape)
+    print(input_image_shape)
+
+    #target_shape = input_image_shape.split(",")
+    #target_shape = [i for i in target_shape]
+    #print(target_shape)
+    # find correct dimensions of input image to be 
+    # used by the model
+    im_test_data, shape_vals = find_dim_order(input_image_shape, test_data)
+
+    model = torch.load(model_path)
+    model.eval()
+
     target_dimension = 0
     for param in model.named_parameters():
         print(param[1].shape)
         target_dimension = len(param[1].shape)
         break
-    current_dimension = len(target_shape)
+    current_dimension = len(list(im_test_data.shape))
+
+    slices = tuple(slice(0, s_val) for s_val in shape_vals)
+    # Apply the slices to the reshaped_input
+    im_test_data = im_test_data[slices]
+    exp_test_data = torch.tensor(im_test_data) 
+
     for i in range(target_dimension - current_dimension):
-        target_shape.append(1)
-    re_test_data = torch.tensor(test_data) 
-    for i in range(len(target_shape) - len(test_data.shape)):
-        re_test_data = torch.unsqueeze(re_test_data, i)
-    print(re_test_data.shape)
-
-    reshaped_input = re_test_data
+        exp_test_data = torch.unsqueeze(exp_test_data, i)
+    print(exp_test_data.shape)
 
 
-    for i, shp in enumerate(target_shape[::-1]):
-        if i == 0:
-            reshaped_input = reshaped_input[:shp,:,:,:,:]
-        if i == 1:
-            reshaped_input = reshaped_input[:,:shp,:,:,:]
-        if i == 2:
-            reshaped_input = reshaped_input[:,:,:shp,:,:]
-        if i == 3:
-            reshaped_input = reshaped_input[:,:,:,:shp,:]
-        if i == 4:
-            reshaped_input = reshaped_input[:,:,:,:,:shp]
-    print(reshaped_input.shape)
+    #reshaped_input = re_test_data
+    #s_vals = target_shape
+
+    #slices = tuple(slice(0, s_val) for s_val in s_vals)
+    # Apply the slices to the reshaped_input
+    #reshaped_input = reshaped_input[slices]
+    #if len(reshaped_input.shape) == 3:
+    #    reshaped_input = reshaped_input[:s_vals[0], :s_vals[1], : s_vals[2]]
+    #if len(reshaped_input.shape) == 4:
+    #    reshaped_input = reshaped_input[:s_vals[0], :s_vals[1], : s_vals[2], : s_vals[3]]
+    #if len(reshaped_input.shape) == 5:
+    #    reshaped_input = reshaped_input[:s_vals[0], :s_vals[1], : s_vals[2], :s_vals[3], : s_vals[4]]
+    #print(reshaped_input.shape)
     #test_data = Image.open(input_image_path)
     #test_data = np.array(test_data)
     #test_data = torch.Tensor(test_data)
     
     #test_data = np.array(test_data)
     #test_data = load_tensor(input_image_path)
-    test_data = torch.Tensor(reshaped_input)
+    #test_data = torch.Tensor(reshaped_input)
     #test_data = torch.reshape(test_data, input_param.shape)
-    print("Tiff input:", reshaped_input.shape)
-    model = torch.load(model_path)
-    model.eval()
-    pred_data = model(reshaped_input)
+    #print("Tiff input:", reshaped_input.shape)
+    #reshaped_input = torch.Tensor(reshaped_input.astype(np.float32))
+    #model = torch.load(model_path)
+    #model.eval()
+    pred_data = model(exp_test_data)
     pred_data_output = pred_data.detach().numpy()
     # save original image matrix
     np.save("output_predicted_image_matrix.npy", pred_data_output)
     # reshape predicted image matrix to display
     pred_data = torch.squeeze(pred_data)
-    if len(pred_data.shape) == 3:
-        pred_data = pred_data[0, :, :]
-    elif len(pred_data.shape) == 4:
-        pred_data = pred_data[0, 0, :, :]
-    elif len(pred_data.shape) == 5:
-        pred_data = pred_data[0, 0, 0, :, :]
-    transform = T.ToPILImage()
-    pred_img = transform(pred_data)
-    pred_img.save("output_predicted_image.png")
+    pred_numpy = pred_data.detach().numpy()
+    pred_numpy = pred_numpy * 255
+    pred_numpy = pred_numpy.astype(np.uint8)
+    imageio.v3.imwrite("output_predicted_image.tif", pred_numpy, extension=".tif")
