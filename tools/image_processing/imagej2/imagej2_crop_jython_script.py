9efe60057d1bad2d7d5b5334f93b7d14b1e34f14
Lucille Delisle <lucille.delisle@epfl.ch>
2024-09-19 14:39:33 +0200

    simplify the crop python script

--- tools/image_processing/imagej2/imagej2_crop_jython_script.py
+++ tools/image_processing/imagej2/imagej2_crop_jython_script.py
@@ -28,33 +28,28 @@ image_dims = input_image_plus.getDimensions()
 # Create a copy of the image.
 input_image_plus_copy = input_image_plus.duplicate()
 
-# Determine if crop in CZT is needed:
-if last_channel != 0 or last_slice != 0 or last_frame != 0 or first_channel != 1 or first_slice != 1 or first_frame != 1:
-    print("CROP in CZT")
-    # Replace 0's with default:
-    if last_channel == 0:
-        last_channel = image_dims[2]
-    if last_slice == 0:
-        last_slice = image_dims[3]
-    if last_frame == 0:
-        last_frame = image_dims[4]
-    # Crop in ZCT
-    print(image_dims)
-    input_image_plus_copy = Duplicator().run(input_image_plus_copy, first_channel, last_channel, first_slice, last_slice, first_frame, last_frame)
-    print(input_image_plus_copy.getDimensions())
-
 # Determine if crop in XY is needed:
 if xleft != 0 or width != 0 or ytop != 0 or height != 0:
-    print("CROP in XY")
     # Need to define a ROI
     if width == 0:
         width = image_dims[0] - xleft
     if height == 0:
         height = image_dims[1] - ytop
     input_image_plus_copy.setRoi(xleft, ytop, width, height)
-    print(input_image_plus_copy.getDimensions())
-    input_image_plus_copy = input_image_plus_copy.resize(width, height, "bilinear")
-    print(input_image_plus_copy.getDimensions())
+
+# Replace 0's with default:
+if last_channel == 0:
+    last_channel = image_dims[2]
+if last_slice == 0:
+    last_slice = image_dims[3]
+if last_frame == 0:
+    last_frame = image_dims[4]
+print("Original dimensions:")
+print(image_dims)
+# This will also crop in XY is a ROI has been set
+input_image_plus_copy = Duplicator().run(input_image_plus_copy, first_channel, last_channel, first_slice, last_slice, first_frame, last_frame)
+print("Final dimensions:")
+print(input_image_plus_copy.getDimensions())
 
 # Save the ImagePlus object as a new image.
 IJ.saveAs(input_image_plus_copy, output_datatype, output_filename)
