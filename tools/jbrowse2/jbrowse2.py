13e5bbcf9d8cacd1a936e610f7c9279b88374b39
fubar2 <ross.lazarus@gmail.com>
2024-10-08 16:16:31 +1100

    Idiotic sort error generates broken pif indexes. Pity the built in fails in biocontainers.

--- tools/jbrowse2/jbrowse2.py
+++ tools/jbrowse2/jbrowse2.py
@@ -1136,10 +1136,8 @@ class JbrowseConnector(object):
         usePIF = False  # much faster if indexed remotely or locally
         useuri = data.startswith("http://") or data.startswith("https://")
         if not useuri:
-            # self.symlink_or_copy(os.path.realpath(data), url)
-            # cmd = ["jbrowse", "make-pif", url]
             url = '%s.pif.gz' % tId
-            cmd = "sort -b -n -k1,1 -k2,3 -k3,4 '%s' | bgzip -c > '%s'" % (data, url)
+            cmd = "sort -b -k1,1 -k2,3n -k3,4n '%s' | bgzip -c > '%s'" % (data, url)
             self.subprocess_popen(cmd)
             cmd = ["tabix", "-b", "3", "-e", "4", "-f", url]
             self.subprocess_check_call(cmd)
