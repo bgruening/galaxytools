e6febdff09ebf3256218d981f2cc6deeea8749e6
Anup Kumar <anup.rulez@gmail.com>
2024-10-14 15:09:49 +0000

    add files

---
+++ tools/tabpfn/main.py
@@ -0,0 +1,27 @@
+"""
+Tabular data prediction using TabPFN
+"""
+
+import argparse
+
+import imageio
+import numpy as np
+import torch
+
+
+def model():
+    """
+    
+    """
+    print()
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    #arg_parser.add_argument("-im", "--imaging_model", required=True, help="Input BioImage model")
+    #arg_parser.add_argument("-ii", "--image_file", required=True, help="Input image file")
+    #arg_parser.add_argument("-is", "--image_size", required=True, help="Input image file's size")
+
+    # get argument values
+    #args = vars(arg_parser.parse_args())
+    #model_path = args["imaging_model"]
