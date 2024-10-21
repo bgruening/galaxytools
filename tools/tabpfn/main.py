6048605b93888fa880cda0634ed667ba0306498e
Anup Kumar <anup.rulez@gmail.com>
2024-10-21 15:46:34 +0000

    update

--- tools/tabpfn/main.py
+++ tools/tabpfn/main.py
@@ -3,25 +3,51 @@ Tabular data prediction using TabPFN
 """
 
 import argparse
+import time
 
-import imageio
-import numpy as np
+from sklearn.metrics import accuracy_score
+from tabpfn import TabPFNClassifier
+import pandas as pd
 import torch
 
 
-def model():
+def separate_features_labels(data):
+    df = pd.read_csv(data, sep=",")
+    labels = df.iloc[:, -1]
+    features = df.iloc[:, :-1]
+    print(df)
+    print(features)
+    print(labels)
+    return features, labels
+
+
+def train_evaluate(args):
     """
-    
+    Train TabPFN
     """
-    print()
+    print(args)
+
+    tr_features, tr_labels = separate_features_labels(args["train_data"])
+    te_features, te_labels = separate_features_labels(args["test_data"])
+
+    classifier = TabPFNClassifier(device='cpu', N_ensemble_configurations=32)
+    s_time = time.time()
+    classifier.fit(tr_features, tr_labels)
+    e_time = time.time()
+    print("Time taken by TabPFN for training: {} seconds".format(e_time - s_time))
+    y_eval, p_eval = classifier.predict(te_features, return_winning_probability=True)
+    print('Accuracy', accuracy_score(te_labels, y_eval))
+
+    te_features["true_labels"] = te_labels
+    te_features["pred_labels"] = y_eval
+    te_features.to_csv("output_predicted_data.csv", sep="\t", index=None)
 
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    #arg_parser.add_argument("-im", "--imaging_model", required=True, help="Input BioImage model")
-    #arg_parser.add_argument("-ii", "--image_file", required=True, help="Input image file")
-    #arg_parser.add_argument("-is", "--image_size", required=True, help="Input image file's size")
+    arg_parser.add_argument("-trdata", "--train_data", required=True, help="Train data")
+    arg_parser.add_argument("-tedata", "--test_data", required=True, help="Test data")
 
     # get argument values
-    #args = vars(arg_parser.parse_args())
-    #model_path = args["imaging_model"]
+    args = vars(arg_parser.parse_args())
+    train_evaluate(args)
