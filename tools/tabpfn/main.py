a6e2e809c1a8aa2ec5ddfef41b260550c73cb179
Anup Kumar <anup.rulez@gmail.com>
2025-01-15 11:50:17 +0000

    fix linting error

--- tools/tabpfn/main.py
+++ tools/tabpfn/main.py
@@ -32,7 +32,7 @@ def train_evaluate(args):
     print('Accuracy', accuracy_score(te_labels, y_eval))
     pred_probas_test = classifier.predict_proba(te_features)
     te_features["predicted_labels"] = y_eval
-    te_features.to_csv("output_predicted_data.tabular", sep="\t", index=None)
+    te_features.to_csv("output_predicted_data", sep="\t", index=None)
     precision, recall, thresholds = precision_recall_curve(te_labels, pred_probas_test[:, 1])
     average_precision = average_precision_score(te_labels, pred_probas_test[:, 1])
     plt.figure(figsize=(8, 6))
