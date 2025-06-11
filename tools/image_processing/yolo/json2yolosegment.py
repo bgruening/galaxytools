5b4d2d844c816adf11de4e4d075d9ba76999a428
Yi Sun <yi.sun@embl.de>
2025-06-11 16:32:46 +0200

    change json2yolosegment.py to take folder

--- tools/image_processing/yolo/json2yolosegment.py
+++ tools/image_processing/yolo/json2yolosegment.py
@@ -3,59 +3,61 @@ import json
 import os
 
 
-def convert_json_to_yolo(input_json, save_dir, class_names_file):
+def convert_json_to_yolo(input_dir, save_dir, class_names_file):
 
     with open(class_names_file, 'r') as f:
         class_names = [line.strip() for line in f.readlines()]
 
     class_to_index = {class_name: i for i, class_name in enumerate(class_names)}
 
-    filename = os.path.basename(input_json)
-    with open(input_json, 'r') as file:
-        data = json.load(file)
+    for filename in os.listdir(input_dir):
+        json_filepath = os.path.join(input_dir, filename)
 
-    image_width = data.get('imageWidth')
-    image_height = data.get('imageHeight')
-    if image_width is None or image_height is None:
-        print(f"Skipping {filename}: missing image dimensions.")
-        return
+        with open(json_filepath, 'r') as f:
+            data = json.load(f)
 
-    annotations = data.get('shapes', [])
+        image_width = data.get('imageWidth')
+        image_height = data.get('imageHeight')
+        if image_width is None or image_height is None:
+            print(f"Skipping {filename}: missing image dimensions.")
+            return
 
-    base, _ = os.path.splitext(filename)
-    output_file = f"{base}.txt"
-    output_filepath = os.path.join(save_dir, output_file)
+        annotations = data.get('shapes', [])
 
-    with open(output_filepath, 'w') as f:
-        for annotation in annotations:
-            label = annotation.get('label')
-            class_index = class_to_index[label]
+        base, _ = os.path.splitext(filename)
+        output_file = f"{base}.txt"
+        output_filepath = os.path.join(save_dir, output_file)
 
-            points = annotation.get('points', [])
+        with open(output_filepath, 'w') as f:
+            for annotation in annotations:
+                label = annotation.get('label')
+                class_index = class_to_index[label]
 
-            if not points:
-                print(f"No points found for annotation '{label}', skipping.")
-                continue
+                points = annotation.get('points', [])
 
-            x = [point[0] / image_width for point in points]
-            y = [point[1] / image_height for point in points]
+                if not points:
+                    print(f"No points found for annotation '{label}', skipping.")
+                    continue
 
-            segmentation_points = ['{} {}'.format(x[i], y[i]) for i in range(len(x))]
-            segmentation_points_string = ' '.join(segmentation_points)
-            line = '{} {}\n'.format(class_index, segmentation_points_string)
-            f.write(line)
+                x = [point[0] / image_width for point in points]
+                y = [point[1] / image_height for point in points]
 
-    print(f"Converted annotations saved to: {output_filepath}")
+                segmentation_points = ['{} {}'.format(x[i], y[i]) for i in range(len(x))]
+                segmentation_points_string = ' '.join(segmentation_points)
+                line = '{} {}\n'.format(class_index, segmentation_points_string)
+                f.write(line)
+
+        print(f"Converted annotations saved to: {output_filepath}")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Convert JSON annotations to YOLO segment format.")
-    parser.add_argument('-i', '--input_json', type=str, help='Full path of the AnyLabeling JSON file.')
+    parser.add_argument('-i', '--input_dir', type=str, help='Full path of the folder containing AnyLabeling JSON files.')
     parser.add_argument('-o', '--save_dir', type=str, help='Path to the directory to save converted YOLO files.')
     parser.add_argument('-c', '--class_names_file', type=str, help='Path to the text file containing class names, one per line.')
     args = parser.parse_args()
 
-    convert_json_to_yolo(args.input_json, args.save_dir, args.class_names_file)
+    convert_json_to_yolo(args.input_dir, args.save_dir, args.class_names_file)
 
 
 if __name__ == "__main__":
