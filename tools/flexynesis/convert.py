1bfcc2824a13207219dbefcab8088b3b64c06510
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2025-07-03 10:26:48 +0200

    add csv to tabular

--- tools/flexynesis/convert.py
+++ tools/flexynesis/convert.py
@@ -11,5 +11,17 @@ def tabular_to_csv(tabular_file, csv_file):
     data.to_csv(csv_file, index=False)
 
 
+def csv_to_tabular(csv_file, tabular_file):
+    """Convert CSV to tabular (TSV)"""
+    data = pd.read_csv(csv_file)
+    data.to_csv(tabular_file, sep="\t", index=False)
+
+
 if __name__ == "__main__":
-    tabular_to_csv(sys.argv[1], sys.argv[2])
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if input_file.endswith('.csv'):
+        csv_to_tabular(input_file, output_file)
+    else:
+        tabular_to_csv(input_file, output_file)
