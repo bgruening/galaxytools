8ad7502142c6a4b6ad8555dbf89a230dd7991a0d
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2025-07-03 23:23:03 +0200

    fix python lint

--- tools/flexynesis/index_to_name.py
+++ tools/flexynesis/index_to_name.py
@@ -4,16 +4,18 @@ import sys
 
 import pandas as pd
 
+
 def get_column_name(file_path, index):
     """
     Get the column name based on an index from a tabular file.
     """
     data = pd.read_csv(file_path, sep=",")
-    index = index-1  # Convert to zero-based index
+    index = index - 1  # Convert to zero-based index
     if index < 0 or index >= len(data.columns):
         print(f"Error: Index {index+1} is out of range. File has {len(data.columns)} columns (1-{len(data.columns)}).")
         return None
     return data.columns[index].strip()
+
 
 if __name__ == "__main__":
 
