a999c19ee975b857d2e0cde65065d5c03b1fd4c0
Anup Kumar <anuprulez@gmail.com>
2026-09-17 16:24:40 +0200

    export encoded vocabulary

--- tools/sklearn/label_encoder.py
+++ tools/sklearn/label_encoder.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 
-def main(inputs, infile, outfile):
+def main(inputs, infile, outfile, outfile_encoding_vocabulary):
     """
     Parameter
     ---------
@@ -19,6 +19,9 @@ def main(inputs, infile, outfile):
 
     outfile : str
         File path to output vector
+
+    outfile_encoding_vocabulary : str
+        File path to output encoding vocabulary
 
     """
     warnings.simplefilter("ignore")
@@ -37,12 +40,17 @@ def main(inputs, infile, outfile):
 
     np.savetxt(outfile, output_vector, fmt="%d", delimiter="\t")
 
+    vocab = {label: index for index, label in enumerate(le.classes_)}
+
+    with open(outfile_encoding_vocabulary, "w") as vocab_handler:
+        json.dump(vocab, vocab_handler)
 
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser()
     aparser.add_argument("-i", "--inputs", dest="inputs", required=True)
     aparser.add_argument("-y", "--infile", dest="infile")
     aparser.add_argument("-o", "--outfile", dest="outfile")
+    aparser.add_argument("-v", "--outfile_encoding_vocabulary", dest="outfile_encoding_vocabulary")
     args = aparser.parse_args()
 
-    main(args.inputs, args.infile, args.outfile)
+    main(args.inputs, args.infile, args.outfile, args.outfile_encoding_vocabulary)
